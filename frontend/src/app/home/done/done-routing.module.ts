6927d29670c7cf7f2bce91364bde0ea2f3879b3a
Denis Ribica <denis.ribica@studium.uni-erlangen.de>
2020-05-05 20:18:16 +0200

    Created a first Hello World Programm. Used some Ionic featueres for input,forms and buttons on the HomePage to get some input an interact with the user as an example. Created another Page and tried some routing and getting the input of the Homepage. Unfortunately i needed to stop the organization of the imports, because there is a bug in vscode on mac.

--- frontend/src/app/home/done/done-routing.module.ts
+++ frontend/src/app/home/done/done-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { DonePage } from './done.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage,
-  },
-  {
-    path: 'done/:name',
-    loadChildren: () =>
-      import('./done/done.module').then((m) => m.DonePageModule),
+    component: DonePage,
   },
 ];
 
@@ -19,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class HomePageRoutingModule {}
+export class DonePageRoutingModule {}
