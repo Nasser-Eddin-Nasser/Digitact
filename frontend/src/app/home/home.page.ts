e89aa365db61deaa8d4d064d28e9d726527da4c3
Denis Ribica <denis.ribica@studium.uni-erlangen.de>
2020-05-15 12:28:34 +0200

    Implemented basic start page

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,33 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
-
-import { FormControl, FormGroup } from '../common/forms/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  checkOutForm = new FormGroup<CheckOutFormModel>({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-  });
-
-  constructor(private navController: NavController) {}
-
-  myS(): void {
-    console.log('firstName', this.checkOutForm.controls.firstName.value);
-
-    this.navController.navigateForward([
-      '/home',
-      'done',
-      this.checkOutForm.controls.firstName.value,
-    ]);
-  }
-}
-
-interface CheckOutFormModel {
-  firstName: string;
-  lastName: string;
-}
+export class HomePage {}
