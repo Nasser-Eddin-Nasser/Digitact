a3139c2bc031dda7778a6428520bf1d32ee10c99
dalex135 <don6alex135@gmail.com>
2020-05-20 01:10:06 +0200

    Latest
    Latest

--- frontend/src/app/forms/forms.page.ts
+++ frontend/src/app/forms/forms.page.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Validators } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-=======
 /*
 @Author
 Bharathwaj Ravi
@@ -16,16 +11,12 @@ Add modifiers under @Modifiers
 */
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
-<<<<<<< HEAD
-=======
 import { BasicInfo } from '../interfaces/basic-info';
 
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
 import { FormControl, FormGroup } from './../common/forms/forms';
 
 @Component({
@@ -34,12 +25,9 @@ import { FormControl, FormGroup } from './../common/forms/forms';
   styleUrls: ['./forms.page.scss'],
 })
 export class FormsPage implements OnInit, OnDestroy {
-<<<<<<< HEAD
-=======
   /*
   @Usage this array holds the object with   information required for different views.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   sideMenuList = [
     {
       id: 1,
@@ -48,23 +36,11 @@ export class FormsPage implements OnInit, OnDestroy {
       isActive: false,
       isCompleted: false,
     },
-<<<<<<< HEAD
-    {
-      id: 2,
-      displayName: 'Contact info',
-      selector: 'form-contact-info',
-      isActive: false,
-      isCompleted: false,
-    },
-  ];
-
-=======
   ];
 
   /*
   @Usage this  object holds current view information.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   currentMenu: {
     id: number;
     displayName: string;
@@ -73,14 +49,6 @@ export class FormsPage implements OnInit, OnDestroy {
     selector: string;
   };
 
-<<<<<<< HEAD
-  formBuilder: FormBuilder;
-
-  totalSteps: number;
-
-  progressPercentage: number;
-
-=======
   /*
   @Usage  holds total steps in the form.
   */
@@ -94,7 +62,6 @@ export class FormsPage implements OnInit, OnDestroy {
   /*
   @Usage  holds all the subscription which will be useful for un subscribing on destroy.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   private subscriptions: Subscription[] = [];
 
   constructor(
@@ -103,49 +70,18 @@ export class FormsPage implements OnInit, OnDestroy {
     private router: Router
   ) {}
 
-<<<<<<< HEAD
-=======
   /*
   @Usage  This property holds the type safe form group fields for basic information view.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   basicInfoObj = new FormGroup<BasicInfo>({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     salutation: new FormControl('mr'),
   });
 
-<<<<<<< HEAD
-  contactInfoObj = new FormGroup<ContactInfo>({
-    phoneNumber: new FormControl(''),
-    eMail: new FormControl(''),
-    linkedIn: new FormControl(''),
-    xing: new FormControl(''),
-  });
-
-  /*
-  contactInfoObj = this.formBuilder.group({
-    phoneNumber: new FormControl(
-      '',
-      Validators.compose([Validators.required, Validators.pattern('^[0-9]+$')])
-    ),
-    eMail: new FormControl(
-      '',
-      Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
-      ])
-    ),
-    linkedIn: new FormControl(''),
-    xing: new FormControl(''),
-  });
-  */
-
-=======
   /*
   @Usage In this method route change is observed and handling is done.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   ngOnInit(): void {
     this.totalSteps = this.sideMenuList.length;
     this.progressPercentage = 0;
@@ -169,12 +105,9 @@ export class FormsPage implements OnInit, OnDestroy {
     this.subscriptions.push(subscription);
   }
 
-<<<<<<< HEAD
-=======
   /*
   @Usage In this method navigation to next step is handled.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   navigateToNextForm(): void {
     if (this.currentMenu.id !== this.totalSteps) {
       const nextMenuIndex = this.sideMenuList.indexOf(this.currentMenu) + 1;
@@ -184,12 +117,9 @@ export class FormsPage implements OnInit, OnDestroy {
     }
   }
 
-<<<<<<< HEAD
-=======
   /*
   @Usage In this method un subscribe event and restore to default values are handled.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   ngOnDestroy(): void {
     for (const subscription of this.subscriptions) {
       subscription.unsubscribe();
@@ -199,32 +129,18 @@ export class FormsPage implements OnInit, OnDestroy {
       lastName: new FormControl(''),
       salutation: new FormControl('mr'),
     });
-<<<<<<< HEAD
-    this.contactInfoObj = new FormGroup<ContactInfo>({
-      phoneNumber: new FormControl('', Validators.maxLength(12)),
-      eMail: new FormControl('', Validators.email),
-      linkedIn: new FormControl(''),
-      xing: new FormControl(''),
-    });
-  }
-
-=======
   }
 
   /*
   @Usage In this method navigation to home page is handled.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   onClose(): void {
     this.navigationController.navigateForward(['/home']);
   }
 
-<<<<<<< HEAD
-=======
   /*
   @Usage In this method navigation to respective step is handled.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   onFormStepsNavigation(event: {
     id: number;
     displayName: string;
@@ -240,13 +156,10 @@ export class FormsPage implements OnInit, OnDestroy {
       },
     });
   }
-<<<<<<< HEAD
-=======
 
   /*
   @Usage In this methos progress values are updated.
   */
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   pageProgressStatusCallBack(event: {
     id: number;
     displayName: string;
@@ -260,19 +173,3 @@ export class FormsPage implements OnInit, OnDestroy {
     this.progressPercentage = completedStep / this.totalSteps;
   }
 }
-<<<<<<< HEAD
-
-interface BasicInfo {
-  firstName: string;
-  lastName: string;
-  salutation: string;
-}
-
-interface ContactInfo {
-  phoneNumber: string;
-  eMail: string;
-  linkedIn: string;
-  xing: string;
-}
-=======
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
