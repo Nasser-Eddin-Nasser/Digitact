dc9bf8a1ab9a24e82a9bc7befd7bce1bd84438e8
dalex135 <don6alex135@gmail.com>
2020-05-20 09:01:54 +0200

    Latest
    Latest

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -7,18 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-<<<<<<< HEAD
-
-  constructor(private router: Router) { }
-
-  onStartApplication(): void {
-    this.router.navigate(['/forms'], { queryParams: { step: 1 } });
-
-=======
   constructor(private router: Router) {}
 
   onStartApplication(): void {
     this.router.navigate(['/forms'], { queryParams: { step: 1 } });
->>>>>>> a40cdd678f8684ffbf0ed13799cb2e793f90f684
   }
 }
