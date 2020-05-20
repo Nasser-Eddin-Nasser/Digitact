040f1e3eb90c0f477263c9ccb32cc590991b3e9b
vishwasanavatti <59308438+vishwasanavatti@users.noreply.github.com>
2020-05-20 23:06:20 +0200

    created a submit page

---
+++ frontend/src/app/forms/steps/submit-page/submit-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-page',
+  templateUrl: './submit-page.component.html',
+  styleUrls: ['./submit-page.component.scss'],
+})
+export class SubmitPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
