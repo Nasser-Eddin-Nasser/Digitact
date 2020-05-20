50df7fb309f998055587af67eb5f201843bbb6af
dalex135 <don6alex135@gmail.com>
2020-05-20 23:40:11 +0200

    Contact form - Frontend

--- frontend/src/app/interfaces/contact-info.ts
+++ frontend/src/app/interfaces/contact-info.ts
@@ -1,6 +1,16 @@
+/*
+@Author
+Alex, Don
+
+Add modifiers under @Modifiers
+@Modifiers
+
+@Purpose
+  - This interface holds the basic informtion view related fields.
+*/
 export interface ContactInfo {
-    phoneNumber: string;
-    eMail: string;
-    linkedIn: string;
-    xing: string;
-  }
+  phoneNumber: string;
+  eMail: string;
+  linkedIn: string;
+  xing: string;
+}
