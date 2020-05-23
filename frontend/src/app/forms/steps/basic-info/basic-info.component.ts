e737742ffcf9ee27d44da4658f3cf4b22b5cb90b
Bharathwaj-Ravi-1995 <bharathravi1995@gmail.com>
2020-05-23 11:52:57 +0530

    Unsubscribing events handled on destroy

--- frontend/src/app/forms/steps/basic-info/basic-info.component.ts
+++ frontend/src/app/forms/steps/basic-info/basic-info.component.ts
@@ -2,7 +2,15 @@
  * @description
  *  This component renders the basic information step view and it's actions.
  */
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { FormGroup } from './../../../common/forms/forms';
 import { BasicInfo } from './../../../interfaces/basic-info';
@@ -12,7 +20,7 @@ import { BasicInfo } from './../../../interfaces/basic-info';
   templateUrl: './basic-info.component.html',
   styleUrls: ['./basic-info.component.scss'],
 })
-export class BasicInfoComponent implements OnInit {
+export class BasicInfoComponent implements OnInit, OnDestroy {
   /*
    * It holds the current menu object from parent
    */
@@ -44,17 +52,34 @@ export class BasicInfoComponent implements OnInit {
   ];
 
   /*
+   * Holds all the subscription which will be useful for un subscribing on destroy.
+   */
+  private subscriptions: Subscription[] = [];
+
+  /*
    * Value changes are observed to update completion status and event is emitted.
    */
   ngOnInit(): void {
-    this.basicInfoObject.valueChanges.subscribe((changedValue) => {
-      this.menuObject.isCompleted =
-        changedValue.salutation.length &&
-        changedValue.firstName.length &&
-        changedValue.lastName.length
-          ? true
-          : false;
-      this.updatedMenuCompletionStatus.emit();
-    });
+    const subscription = this.basicInfoObject.valueChanges.subscribe(
+      (changedValue) => {
+        this.menuObject.isCompleted =
+          changedValue.salutation.length &&
+          changedValue.firstName.length &&
+          changedValue.lastName.length
+            ? true
+            : false;
+        this.updatedMenuCompletionStatus.emit();
+      }
+    );
+    this.subscriptions.push(subscription);
+  }
+
+  /*
+   * In this method un subscribe event is handled.
+   */
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
   }
 }
