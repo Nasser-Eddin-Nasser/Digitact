dcf1ea2cf2d505edecac8f75815bb8d8b109701e
dalex135 <don6alex135@gmail.com>
2020-05-23 17:05:52 +0200

    Comments added

--- frontend/src/app/forms/steps/contact-info/contact-info.component.ts
+++ frontend/src/app/forms/steps/contact-info/contact-info.component.ts
@@ -1,14 +1,16 @@
-/*
-@Author
-Alex, Don
-
-Add modifiers under @Modifiers
-@Modifiers
-
-@Purpose
-  - This component renders the contact information step view and it's actions.
-*/
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+/**
+ * @description
+ *  This component renders the contact information step view and it's actions.
+ */
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { FormGroup } from './../../../common/forms/forms';
 import { ContactInfo } from './../../../interfaces/contact-info';
@@ -18,7 +20,10 @@ import { ContactInfo } from './../../../interfaces/contact-info';
   templateUrl: './contact-info.component.html',
   styleUrls: ['./contact-info.component.scss'],
 })
-export class ContactInfoComponent implements OnInit {
+export class ContactInfoComponent implements OnInit, OnDestroy {
+  /**
+   * It holds the current menu object from parent
+   */
   @Input() menuObject: {
     id: number;
     displayName: string;
@@ -26,25 +31,44 @@ export class ContactInfoComponent implements OnInit {
     isActive: boolean;
     selecteor: string;
   };
+
+  /**
+   * It holds typesafe form group property fields.
+   */
   @Input() contactInfoObject: FormGroup<ContactInfo>;
 
-  @Output() pageProgressStatusCallBack = new EventEmitter();
+  /**
+   * It holds the array objects of drop down menu.
+   */
+  @Output() updatedMenuCompletionStatus = new EventEmitter();
 
-  constructor() {}
+  /**
+   * Holds all the subscription which will be useful for un subscribing on destroy.
+   */
+  private subscriptions: Subscription[] = [];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const subscription = this.contactInfoObject.valueChanges.subscribe(
+      (changedValue) => {
+        this.menuObject.isCompleted =
+          changedValue.phoneNumber.length &&
+          changedValue.eMail.length &&
+          changedValue.linkedIn.length &&
+          changedValue.xing.length
+            ? true
+            : false;
+        this.updatedMenuCompletionStatus.emit();
+      }
+    );
+    this.subscriptions.push(subscription);
+  }
 
-  onValChange(): void {
-    if (
-      this.contactInfoObject.controls.phoneNumber.value.length &&
-      this.contactInfoObject.controls.eMail.value.length &&
-      this.contactInfoObject.controls.linkedIn.value.length &&
-      this.contactInfoObject.controls.xing.value.length
-    ) {
-      this.menuObject.isCompleted = true;
-    } else {
-      this.menuObject.isCompleted = false;
+  /**
+   * In this method un subscribe event is handled.
+   */
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
     }
-    this.pageProgressStatusCallBack.emit(this.menuObject);
   }
 }
