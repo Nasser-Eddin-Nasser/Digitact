f614891b89afd87937db49898b0ab9fce47e55a7
dalex135 <don6alex135@gmail.com>
2020-05-23 16:56:25 +0200

    Change of Comments and Adding Observers to Contact Info

--- frontend/src/app/forms/side-menu/side-menu.component.ts
+++ frontend/src/app/forms/side-menu/side-menu.component.ts
@@ -1,13 +1,7 @@
-/*
-@Author
-Bharathwaj Ravi
-
-Add modifiers under @Modifiers
-@Modifiers
-
-@Purpose
-  - This component renders the hamburger side menu and its actions.
-*/
+/**
+ * @description
+ *  This component renders the hamburger side menu and its actions.
+ */
 
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
@@ -17,9 +11,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent {
-  /*
-  @Usage this array holds the list of different steps information.
-  */
+  /**
+   * This array holds the list of different steps information.
+   */
   @Input() sideMenuList: Array<{
     id: number;
     displayName: string;
@@ -28,23 +22,22 @@ export class SideMenuComponent {
     selector: string;
   }>;
 
-  /*
-  @Usage this takes care of emitting event to parent.
-  */
-  @Output() private pageProgressStatusCallBack = new EventEmitter();
+  /**
+   * This takes care of emitting event to parent.
+   */
+  @Output() private chosenStep = new EventEmitter();
 
-  constructor() {}
-
-  /*
-  @Usage this method emits the event for progrss call back.
-  */
-  onMenuChange(menu: {
+  /**
+   * This method emits the event for step navigation.
+   * @param  menu - Contains the menu to navigate
+   */
+  changeStep(menu: {
     id: number;
     displayName: string;
     isCompleted: boolean;
     isActive: boolean;
     selector: string;
   }): void {
-    this.pageProgressStatusCallBack.emit(menu);
+    this.chosenStep.emit(menu);
   }
 }
