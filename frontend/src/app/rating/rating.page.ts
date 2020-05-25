c26653abd0b41ddf8213f6e7f96f679cdc240097
MBuchalik <dev@martin-buchalik.de>
2020-05-25 13:10:07 +0200

    Make the rating page look more like in the mockups. Add some global styles. Implement basic side menu.

--- frontend/src/app/rating/rating.page.ts
+++ frontend/src/app/rating/rating.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 import { FormControl, FormGroup } from '../common/forms/forms';
 
@@ -10,12 +12,18 @@ import { ApplicantScore, RatingForm } from './model/rating-form.model';
   styleUrls: ['./rating.page.scss'],
 })
 export class RatingPage {
+  constructor(private navController: NavController) {}
+
   ratingForm = new FormGroup<RatingForm>({
     applicantScore: new FormGroup<ApplicantScore>({
-      rhetoric: new FormControl(),
-      motivation: new FormControl(),
-      selfAssurance: new FormControl(),
-      personalImpression: new FormControl(),
+      rhetoric: new FormControl(undefined, Validators.required),
+      motivation: new FormControl(undefined, Validators.required),
+      selfAssurance: new FormControl(undefined, Validators.required),
+      personalImpression: new FormControl(undefined, Validators.required),
     }),
   });
+
+  goToHomePage(): void {
+    this.navController.navigateBack('/home');
+  }
 }
