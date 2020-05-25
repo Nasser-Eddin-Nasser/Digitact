e09ccdb877cc3ef3abe91eb675991d8b99ce3436
vishwasanavatti <59308438+vishwasanavatti@users.noreply.github.com>
2020-05-25 19:57:52 +0200

    database access for HR-Monitor

--- HR-Monitor/src/Model/StorageModel.java
+++ HR-Monitor/src/Model/StorageModel.java
@@ -3,16 +3,19 @@ package Model;
 import Main.App;
 import Model.User.User;
 import Storage.Dummy;
+import dao.UserDao;
 
 import java.util.List;
 
-
 public class StorageModel {
 
-    public List<User> getDB() {
-        if (App.DEVELOPMENT_ENVIRONMENT) {
-            return Dummy.DB;
-        }
-        return null;
-    }
+	public List<User> getDB() {
+		if (App.DEVELOPMENT_ENVIRONMENT) {
+			return Dummy.DB;
+		} else {
+			UserDao dao = new UserDao();
+			return dao.getUsers();
+		}
+		// return null;
+	}
 }
