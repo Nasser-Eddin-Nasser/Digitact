85cdfdd09d2623dfd04cb9cd0b435d22930cc4bb
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-05-26 10:05:26 +0200

    add DB Config sheet

---
+++ HR-Monitor/src/Database/Configuration.java
@@ -0,0 +1,7 @@
+package Database;
+
+public class Configuration {
+    public static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    public static  final String user = "postgres";
+    public static  final String password = "postgres";
+}
