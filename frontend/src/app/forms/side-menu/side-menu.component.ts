b60d5290bb9388e36673d6ffd5297e292d3e3664
MBuchalik <dev@martin-buchalik.de>
2020-05-28 16:46:19 +0200

    Update the main form structure to use and rely on Validators. Add multiple small enhancements based on the new functionality.

--- frontend/src/app/forms/side-menu/side-menu.component.ts
+++ frontend/src/app/forms/side-menu/side-menu.component.ts
@@ -1,9 +1,14 @@
-/**
- * @description
- *  This component renders the hamburger side menu and its actions.
- */
+/*
+  @description
+    This component renders the hamburger side menu and its actions.
+*/
 
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { FormGroup } from '../../common/forms/forms';
+import { FormsData } from '../../model/forms-data.model';
+import { ApplicationStep, ApplicationStepsArr } from '../model/steps.model';
 
 @Component({
   selector: 'app-side-menu',
@@ -11,33 +16,46 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent {
-  /**
-   * This array holds the list of different steps information.
-   */
-  @Input() sideMenuList: Array<{
-    id: number;
-    displayName: string;
-    isCompleted: boolean;
-    isActive: boolean;
-    selector: string;
-  }>;
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   /**
-   * This takes care of emitting event to parent.
+   * Make the Steps available in the template.
+   *
+   * Only access this property in the template.
+   * In the TS file, you can directly refer to the underlying element.
    */
-  @Output() private chosenStep = new EventEmitter();
+  readonly APPLICATION_STEP = ApplicationStep;
 
   /**
-   * This method emits the event for step navigation.
-   * @param  menu - Contains the menu to navigate
+   * Make the Steps Array available in the template.
+   *
+   * Only access this property in the template.
+   * In the TS file, you can directly refer to the underlying element.
    */
-  changeStep(menu: {
-    id: number;
-    displayName: string;
-    isCompleted: boolean;
-    isActive: boolean;
-    selector: string;
-  }): void {
-    this.chosenStep.emit(menu);
+  readonly APPLICATION_STEP_ARR = ApplicationStepsArr;
+
+  /**
+   * Data of the entire form.
+   */
+  @Input()
+  formsData: FormGroup<FormsData>;
+
+  /**
+   * Which step is currently displayed?
+   */
+  @Input()
+  currentStep: ApplicationStep;
+
+  /**
+   * Update the "step" query paremeter.
+   * You can use this to navigate between the different form steps!
+   */
+  navigateToStep(step: ApplicationStep): void {
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: {
+        step,
+      },
+    });
   }
 }
