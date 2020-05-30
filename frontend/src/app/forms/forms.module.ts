482e5ffdf5f65a738c7a1e22c3eb9e8749c5cdc3
dalex135 <don6alex135@gmail.com>
2020-05-30 18:43:15 +0200

    Basic GUI design with modal

--- frontend/src/app/forms/forms.module.ts
+++ frontend/src/app/forms/forms.module.ts
@@ -13,6 +13,7 @@ import { FormsPage } from './forms.page';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { BasicInfoComponent } from './steps/basic-info/basic-info.component';
 import { ContactInfoComponent } from './steps/contact-info/contact-info.component';
+import { EducationInfoEntryComponent } from './steps/education-info-entry/education-info-entry.component';
 import { EducationInfoComponent } from './steps/education-info/education-info.component';
 import { SubmitPageComponent } from './steps/submit-page/submit-page.component';
 
@@ -31,6 +32,7 @@ import { SubmitPageComponent } from './steps/submit-page/submit-page.component';
     SubmitPageComponent,
     ContactInfoComponent,
     EducationInfoComponent,
+    EducationInfoEntryComponent,
   ],
 })
 export class FormsPageModule {}
