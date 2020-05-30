4f80454658f24a46eeb87c34e7adfc24050e9ed4
dalex135 <don6alex135@gmail.com>
2020-05-30 18:43:15 +0200

    Basic GUI design with modal

---
+++ frontend/src/app/forms/steps/education-info-entry/education-info-entry.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'forms-education-info-entry',
+  templateUrl: './education-info-entry.component.html',
+  styleUrls: ['./education-info-entry.component.scss'],
+})
+export class EducationInfoEntryComponent {}
--- frontend/src/app/forms/steps/education-info/education-info.component.ts
+++ frontend/src/app/forms/steps/education-info/education-info.component.ts
@@ -3,9 +3,11 @@
     This component renders the education information step view and its actions.
 */
 import { Component, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 import { FormGroup } from '../../../common/forms/forms';
 import { FormsData } from '../../../model/forms-data.model';
+import { EducationInfoEntryComponent } from '../education-info-entry/education-info-entry.component';
 
 @Component({
   selector: 'form-education-info',
@@ -13,9 +15,26 @@ import { FormsData } from '../../../model/forms-data.model';
   styleUrls: ['./education-info.component.scss'],
 })
 export class EducationInfoComponent {
-  constructor() {
-    console.log('This is EducationInfoComponent');
-  }
   @Input()
   formsData: FormGroup<FormsData>;
+
+  /**
+   * Constructor
+   */
+  constructor(public modalController: ModalController) {
+    console.log('This is EducationInfoComponent');
+  }
+
+  /**
+   * Open education info entry
+   */
+  // tslint:disable-next-line: typedef
+  async presentEduInfo() {
+    const modal = await this.modalController.create({
+      component: EducationInfoEntryComponent,
+      swipeToClose: true,
+      presentingElement: await this.modalController.getTop(),
+    });
+    return await modal.present();
+  }
 }
