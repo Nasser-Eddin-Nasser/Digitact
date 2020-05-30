4bcbe92a33c452379e88a8b3d9e52460feaf5cc3
dalex135 <don6alex135@gmail.com>
2020-05-30 14:53:51 +0200

    Edu-info page added

---
+++ frontend/src/app/forms/steps/education-info/education-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-education-info',
+  templateUrl: './education-info.component.html',
+  styleUrls: ['./education-info.component.scss'],
+})
+export class EducationInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
