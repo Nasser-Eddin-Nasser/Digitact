83e026ca27695a90b12393c70c1f7cd7f8aaf9a9
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-05-31 11:58:35 +0200

    formatter

--- Backend/src/main/java/Digitact/Backend/Controller/HRController.java
+++ Backend/src/main/java/Digitact/Backend/Controller/HRController.java
@@ -4,30 +4,29 @@ import Digitact.Backend.Model.Education;
 import Digitact.Backend.Model.User.User;
 import Digitact.Backend.Storage.IDataRepository;
 import Digitact.Backend.Storage.IEducationRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /** This is the controller class of the HR */
 @RequestMapping("api/HRController")
 @RestController
 public class HRController {
-  @Autowired IDataRepository dataRepository;
+    @Autowired IDataRepository dataRepository;
 
-  @Autowired IEducationRepository educationRepository;
+    @Autowired IEducationRepository educationRepository;
 
-  /** @return JSON object of the applicants */
-  @GetMapping("/getApplicants")
-  public List<User> getApplicants() {
-    return new ArrayList<User>(dataRepository.getApplicants());
-  }
+    /** @return JSON object of the applicants */
+    @GetMapping("/getApplicants")
+    public List<User> getApplicants() {
+        return new ArrayList<User>(dataRepository.getApplicants());
+    }
 
-  @GetMapping("/getAllEducationsInfo")
-  public List<Education> getFullEducationsInfo() {
-    return new ArrayList<Education>(educationRepository.getAllEducationsInfo());
-  }
+    @GetMapping("/getAllEducationsInfo")
+    public List<Education> getFullEducationsInfo() {
+        return new ArrayList<Education>(educationRepository.getAllEducationsInfo());
+    }
 }
