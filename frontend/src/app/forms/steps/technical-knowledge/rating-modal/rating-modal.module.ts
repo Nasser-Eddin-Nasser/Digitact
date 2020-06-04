a5da8f0d4eb99ee0356d50b45b503bca02e2f04b
MBuchalik <dev@martin-buchalik.de>
2020-06-04 10:33:26 +0200

    Show the current rating as stars in the list

--- frontend/src/app/forms/steps/technical-knowledge/rating-modal/rating-modal.module.ts
+++ frontend/src/app/forms/steps/technical-knowledge/rating-modal/rating-modal.module.ts
@@ -5,10 +5,17 @@ import { IonicModule } from '@ionic/angular';
 
 import { ItemPopoverComponent } from './item-popover/item-popover.component';
 import { RatingModalComponent } from './rating-modal.component';
+import { RangeToPipe } from './stars/range-to.pipe';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule],
-  declarations: [RatingModalComponent, ItemPopoverComponent],
+  declarations: [
+    RatingModalComponent,
+    ItemPopoverComponent,
+    RangeToPipe,
+    StarsComponent,
+  ],
   exports: [RatingModalComponent],
 })
 export class RatingModalModule {}
---
+++ frontend/src/app/forms/steps/technical-knowledge/rating-modal/stars/range-to.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/**
+ * This **pure** Pipe creates an array containing numbers from `start` to `end`.
+ *
+ * Example usage:
+ *
+ * ```
+ * <ng-container *ngFor="n of 1 | rangeTo 3">
+ *  {{n}},
+ * </ng-container>
+ * ```
+ *
+ * This will output:
+ * ```
+ * 1,2,3,
+ * ```
+ */
+@Pipe({
+  name: 'rangeTo',
+})
+export class RangeToPipe implements PipeTransform {
+  transform(start: number, end: number): number[] {
+    const result: number[] = [];
+    for (let i = start; i <= end; i++) {
+      result.push(i);
+    }
+
+    return result;
+  }
+}
