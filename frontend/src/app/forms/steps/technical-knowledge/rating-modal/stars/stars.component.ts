f7af88babf70fe5cea461166c91fe6c00d5dbf0a
MBuchalik <dev@martin-buchalik.de>
2020-06-04 10:33:26 +0200

    Show the current rating as stars in the list

---
+++ frontend/src/app/forms/steps/technical-knowledge/rating-modal/stars/stars.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-stars',
+  templateUrl: './stars.component.html',
+  styleUrls: ['./stars.component.scss'],
+})
+export class StarsComponent {
+  readonly TOTAL_STARS = 5;
+
+  @Input()
+  filledStars: number;
+}
