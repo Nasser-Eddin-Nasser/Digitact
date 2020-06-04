61cd3ace77157b0844a34807bb442016e08b8776
MBuchalik <dev@martin-buchalik.de>
2020-06-04 11:16:25 +0200

    Render the list of Technical Knowledge categories

--- frontend/src/app/forms/steps/technical-knowledge/technical-knowledge.component.ts
+++ frontend/src/app/forms/steps/technical-knowledge/technical-knowledge.component.ts
@@ -2,11 +2,14 @@
   @description
     This component renders the technical knowledge step view and its actions.
 */
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IonRouterOutlet, ModalController } from '@ionic/angular';
 
-import { FormGroup } from '../../../common/forms/forms';
-import { FormsData } from '../../../model/forms-data.model';
+import { FormArray, FormGroup } from '../../../common/forms/forms';
+import {
+  FormsData,
+  TechnicalKnowledgeEntry,
+} from '../../../model/forms-data.model';
 
 import {
   RatingModalComponent,
@@ -18,27 +21,52 @@ import {
   templateUrl: './technical-knowledge.component.html',
   styleUrls: ['./technical-knowledge.component.scss'],
 })
-export class TechnicalKnowledgeComponent {
+export class TechnicalKnowledgeComponent implements OnInit {
   /**
    * Data of the entire form.
    */
   @Input()
   formsData: FormGroup<FormsData>;
 
+  /**
+   * The items we will later render in the template.
+   *
+   * Important: Currently, we initialize it in ngOnInit and never change it afterwards.
+   * So, if the reference(s) to the form items change in our input, this will not be reflected here.
+   */
+  listItems: ListItem[];
+
   constructor(
     private ionRouterOutlet: IonRouterOutlet,
     private modalController: ModalController
-  ) {
-    // TODO: Just for development purposes.
-    window.setTimeout(() => {
-      this.showRatingModal();
-    });
+  ) {}
+
+  ngOnInit(): void {
+    // This might lead to bugs if the form controls (their references) get changed. But let's ignore that for now.
+
+    this.listItems = [
+      {
+        displayName: 'Professional Software',
+        formItem: this.formsData.controls.technicalKnowledge.controls
+          .professionalSoftware,
+      },
+      {
+        displayName: 'Databases',
+        formItem: this.formsData.controls.technicalKnowledge.controls.databases,
+      },
+      {
+        displayName: 'Programming Languages and Frameworks',
+        formItem: this.formsData.controls.technicalKnowledge.controls
+          .programmingLanguages,
+      },
+    ];
   }
 
-  async showRatingModal(): Promise<void> {
+  async showRatingModal(
+    formItem: FormArray<TechnicalKnowledgeEntry>
+  ): Promise<void> {
     const ratingModalProps: RatingModalProps = {
-      formArray: this.formsData.controls.technicalKnowledge.controls
-        .professionalSoftware,
+      formArray: formItem,
     };
 
     const modal = await this.modalController.create({
@@ -52,4 +80,9 @@ export class TechnicalKnowledgeComponent {
 
     modal.present();
   }
+}
+
+interface ListItem {
+  formItem: FormArray<TechnicalKnowledgeEntry>;
+  displayName: string;
 }
