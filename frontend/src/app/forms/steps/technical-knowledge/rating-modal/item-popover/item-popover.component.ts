9787381c2593f0838d82c1196d9a1a79eaddce29
MBuchalik <dev@martin-buchalik.de>
2020-06-04 11:40:56 +0200

    Add buttons to the popover to close it and to delete the item.

--- frontend/src/app/forms/steps/technical-knowledge/rating-modal/item-popover/item-popover.component.ts
+++ frontend/src/app/forms/steps/technical-knowledge/rating-modal/item-popover/item-popover.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
 
 import { FormGroup } from '../../../../../common/forms/forms';
 import { TechnicalKnowledgeEntry } from '../../../../../model/forms-data.model';
+import { TechnicalKnowledgeFormItemsService } from '../../technical-knowledge-form-items.service';
 
 @Component({
   templateUrl: './item-popover.component.html',
@@ -10,6 +12,20 @@ import { TechnicalKnowledgeEntry } from '../../../../../model/forms-data.model';
 export class ItemPopoverComponent implements ItemPopoverProps {
   @Input()
   formItem: FormGroup<TechnicalKnowledgeEntry>;
+
+  constructor(
+    private popoverController: PopoverController,
+    private techicalKnowledgeFormItemsService: TechnicalKnowledgeFormItemsService
+  ) {}
+
+  closePopover(): void {
+    this.popoverController.dismiss();
+  }
+
+  deleteItem(): void {
+    this.techicalKnowledgeFormItemsService.resetFormItem(this.formItem);
+    this.closePopover();
+  }
 }
 
 export interface ItemPopoverProps {
