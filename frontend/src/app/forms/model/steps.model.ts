672f57c37830a822fac505a376eea683f63e9414
Denis Ribica <denis.ribica@studium.uni-erlangen.de>
2020-06-04 17:44:54 +0200

    Implemented Basic skeleton of picture-adding page

--- frontend/src/app/forms/model/steps.model.ts
+++ frontend/src/app/forms/model/steps.model.ts
@@ -7,6 +7,7 @@
 export enum ApplicationStep {
   BasicInformation = 'basic-information',
   ContactInformation = 'contact-information',
+  PicturesEntry = 'pictures-entry',
   EducationInformation = 'education-information',
   FieldDesignationPreference = 'field-designation-preference',
   Submit = 'submit',
