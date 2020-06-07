fdf9ed4e2fdf0fc811d9d51848d8416855c322df
Vishwas Anavatti <vishwasanavatti@Vishwass-MacBook-Air.local>
2020-06-07 21:43:50 +0200

    application size badge added to home page

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+
+import { StorageHandlerService } from '../services/storage-handler.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor(private navController: NavController) {}
+export class HomePage implements OnInit {
+  applicationSize: number;
+  constructor(
+    private navController: NavController,
+    private storage: StorageHandlerService
+  ) {}
 
   startApplication(): void {
     this.navController.navigateForward(['/forms']);
   }
   goToApplications(): void {
     this.navController.navigateRoot(['/applications']);
+  }
+  ngOnInit(): void {
+    this.storage.getAllItems(this.storage.applicantDetailsDb).then((data) => {
+      this.applicationSize = data.length;
+    });
   }
 }
