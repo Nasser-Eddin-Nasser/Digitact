ee63ad2c28568b87566a4e0fcad644bf2e980102
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-06-08 17:05:27 +0200

    add json converter

--- HR-Monitor/src/Util/JSONTools.java
+++ HR-Monitor/src/Util/JSONTools.java
@@ -1,4 +1,22 @@
 package Util;
 
+import Model.User.ApplicantUI;
+import Storage.DBStorage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+
 public class JSONTools {
+    public static void convertJSONToApplicant(String jsonInput) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        try {
+            DBStorage.setUsers(
+                    mapper.readValue(jsonInput, new TypeReference<List<ApplicantUI>>() {}));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 }
