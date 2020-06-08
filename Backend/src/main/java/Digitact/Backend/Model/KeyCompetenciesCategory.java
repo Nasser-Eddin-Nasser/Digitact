3773c806adc613d348e99da3168a77620b4089c7
Nasser <nasser-eddin.nasser@stud.uni-bamberg.de>
2020-06-08 12:57:58 +0200

    add json property to Key Category enum

--- Backend/src/main/java/Digitact/Backend/Model/KeyCompetenciesCategory.java
+++ Backend/src/main/java/Digitact/Backend/Model/KeyCompetenciesCategory.java
@@ -1,8 +1,14 @@
 package Digitact.Backend.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum KeyCompetenciesCategory {
+  @JsonProperty("languages")
   Languages,
+  @JsonProperty("professionalSoftware")
   ProfessionalSoftware,
+  @JsonProperty("databases")
   Databases,
+  @JsonProperty("programmingLanguagesAndFrameworks")
   ProgrammingLanguagesAndFrameworks
 }
