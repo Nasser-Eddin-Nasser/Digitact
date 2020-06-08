9adcb9ef2222b7f11933ac5543b9e927bf4b48a5
Bharathravi1995 <bharathravi1995@gmail.com>
2020-06-08 23:49:25 +0530

    Alert and Toast controller import changed to wrapper

--- frontend/src/app/rating/rating.page.ts
+++ frontend/src/app/rating/rating.page.ts
@@ -9,10 +9,10 @@ import { Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
-import { ToastController } from '@ionic/angular';
-import { Subscription } from 'rxjs';
 
 import { FormControl, FormGroup } from '../common/forms/forms';
+import { AlertController } from '../common/ion-wrappers/alert-controller';
+import { ToastController } from '../common/ion-wrappers/toast-controller';
 import { hrRatingStep, hrRatingStepArr } from '../forms/model/steps.model';
 import { FormsData } from '../model/forms-data.model';
 import { StorageHandlerService } from '../services/storage-handler.service';
