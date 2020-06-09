b9acd086e532df75f447f472ecbae88a64a15a99
Nasser <nasser-eddin.nasser@stud.uni-bamberg.de>
2020-06-09 20:16:26 +0200

    remove deadcode

--- HR-Monitor/src/Storage/Dummy.java
+++ HR-Monitor/src/Storage/Dummy.java
@@ -1,17 +1,15 @@
 package Storage;
 
-import Model.Education;
 import Model.User.ApplicantUI;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class Dummy {
     public static List<ApplicantUI> DB;
 
-    public static List<Education> EduDB;
 
     public Dummy() {
         DB = new LinkedList<ApplicantUI>();
-        EduDB = new LinkedList<Education>();
     }
 }
