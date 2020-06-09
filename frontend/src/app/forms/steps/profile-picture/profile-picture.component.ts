03fea303dab7486a76a403d9d1364598d1b37b21
denoribi <denis.amberg@gmail.com>
2020-06-09 10:43:56 +0200

    Incorporate feedback

--- frontend/src/app/forms/steps/profile-picture/profile-picture.component.ts
+++ frontend/src/app/forms/steps/profile-picture/profile-picture.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import {
   CameraDirection,
+  CameraOptions,
   CameraResultType,
   CameraSource,
   Plugins,
@@ -19,21 +20,23 @@ export class ProfilePictureComponent {
   @Input()
   formsData: FormGroup<FormsData>;
 
-  async takePicture(): Promise<void> {
-    const image = await Plugins.Camera.getPhoto({
+  takePicture(): void {
+    const cameraConfig: CameraOptions = {
       quality: 100,
       allowEditing: false,
       resultType: CameraResultType.Base64,
       source: CameraSource.Camera,
       direction: CameraDirection.Front,
-    }).catch((err) => {
-      console.log('catch cameraphoto');
-      throw new Error(err);
-    });
-
-    this.formsData.controls.profilePicture.controls.pictureBase64.setValue(
-      'data:image/jpeg;base64,' + image.base64String
-    );
+    };
+    Plugins.Camera.getPhoto(cameraConfig)
+      .then((result) => {
+        this.formsData.controls.profilePicture.controls.pictureBase64.setValue(
+          'data:image/jpeg;base64,' + result.base64String
+        );
+      })
+      .catch(() => {
+        // It looks like closing the Camera overlay in the webview, rejects the promise
+      });
   }
   /**
    * delete picture entry and reset page so you can add a new picture.
