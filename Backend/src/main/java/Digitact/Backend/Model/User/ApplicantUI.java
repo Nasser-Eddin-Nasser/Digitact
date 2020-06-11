c72d138e47a256654082972cd9f92d3da33cb357
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-06-11 09:49:00 +0200

    modify applicant model to fit json and DB schema

--- Backend/src/main/java/Digitact/Backend/Model/User/ApplicantUI.java
+++ Backend/src/main/java/Digitact/Backend/Model/User/ApplicantUI.java
@@ -1,60 +1,90 @@
 package Digitact.Backend.Model.User;
 
 import Digitact.Backend.Model.Education;
+import Digitact.Backend.Model.Image.ImageString;
 import Digitact.Backend.Model.Industries;
 import Digitact.Backend.Model.KeyCompetence;
 import Digitact.Backend.Model.Positions;
+
 import java.util.List;
 
 public class ApplicantUI implements IUser {
 
-    private String firstName;
-    private String lastName;
-    private List<Education> educations;
-    private Industries industries;
-    private Positions positions;
-    private List<KeyCompetence> keyCompetencies;
+  private String firstName;
+  private String lastName;
 
-    protected ApplicantUI() {}
+  private String email;
+  private String phone;
 
-    public ApplicantUI(
-            String firstName,
-            String lastName,
-            List<Education> educations,
-            Industries industries,
-            Positions positions,
-            List<KeyCompetence> keyCompetencies) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.educations = educations;
-        this.industries = industries;
-        this.positions = positions;
-        this.keyCompetencies = keyCompetencies;
-    }
+  private List<ImageString> imageList;
+  private List<Education> educations;
 
-    public Industries getIndustries() {
-        return industries;
-    }
+  private List<Industries> industries;
+  private List<Positions> positions;
+  private List<KeyCompetence> keyCompetencies;
 
-    public Positions getPositions() {
-        return positions;
-    }
+  protected ApplicantUI() {}
 
-    public List<Education> getEducations() {
-        return educations;
-    }
+  public ApplicantUI(
+      String firstName,
+      String lastName,
+      String email,
+      String phone,
+      List<ImageString> imageList,
+      List<Education> educations,
+      List<Industries> industries,
+      List<Positions> positions,
+      List<KeyCompetence> keyCompetencies) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.imageList = imageList;
+    this.educations = educations;
+    this.industries = industries;
+    this.positions = positions;
+    this.keyCompetencies = keyCompetencies;
+  }
 
-    public List<KeyCompetence> getKeyCompetencies() {
-        return keyCompetencies;
-    }
+  public List<Positions> getPositions() {
+    return positions;
+  }
 
-    @Override
-    public String getFirstName() {
-        return firstName;
-    }
+  public List<Education> getEducations() {
+    return educations;
+  }
 
-    @Override
-    public String getLastName() {
-        return lastName;
-    }
+  public List<KeyCompetence> getKeyCompetencies() {
+    return keyCompetencies;
+  }
+
+  public List<Industries> getIndustries() {
+    return industries;
+  }
+
+  public List<ImageString> getImageList() {
+    return imageList;
+  }
+
+  public void setImageList(List<ImageString> imageList) {
+    this.imageList = imageList;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  @Override
+  public String getFirstName() {
+    return firstName;
+  }
+
+  @Override
+  public String getLastName() {
+    return lastName;
+  }
 }
