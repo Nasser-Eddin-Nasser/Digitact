758c3ae544939c22f67fc95cefa7a83a7bfc4960
Nasser <nasser-eddin.nasser@stud.uni-bamberg.de>
2020-06-11 09:51:08 +0200

    reformat and extend ClientController

--- Backend/src/main/java/Digitact/Backend/Controller/ClientController.java
+++ Backend/src/main/java/Digitact/Backend/Controller/ClientController.java
@@ -1,47 +1,50 @@
 package Digitact.Backend.Controller;
 
-import Digitact.Backend.Model.User.*;
+import Digitact.Backend.Model.User.Admin;
+import Digitact.Backend.Model.User.ApplicantUI;
+import Digitact.Backend.Model.User.User;
+import Digitact.Backend.Model.User.UserUI;
 import Digitact.Backend.Storage.IDataRepository;
-import java.util.List;
+import Digitact.Backend.Storage.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /** This is the controller class of the Clients */
 @RequestMapping("api/controller")
 @RestController
 public class ClientController {
+  @Autowired IDataRepository repository;
 
-    @Autowired IDataRepository repository;
+  //  @PostMapping("/setImage")
+  //  public String setImage(@RequestBody String image) {
+  //    ImageTool it = new ImageTool(repository);
+  //    it.createAppImage(image, ImageType.CV);
+  //    return "image is created in the database";
+  //  }
 
-    /** @return JSON object of the user */
-    @GetMapping("/getAll")
-    public List<User> getAll() {
-        return repository.findAll();
-    }
+  /** @return JSON object of the user */
+  @GetMapping("/getAll")
+  public List<User> getAll() {
+    return repository.findAll();
+  }
 
-    /**
-     * @param user - JSON request's user object
-     *     <p>save the user "Applicant" in the DB using repository
-     * @return "Applicant is created in the database"
-     */
-    @PostMapping("/createApplicant")
-    public String createApplicant(@RequestBody ApplicantUI user) {
-        Applicant app = new Applicant(user.getFirstName(), user.getLastName());
-        user.getEducations().forEach(x -> x.setUser(app));
-        user.getIndustries().setUser(app);
-        user.getPositions().setUser(app);
-        user.getKeyCompetencies().forEach(x -> x.setUser(app));
-        app.setEducations(user.getEducations());
-        app.setIndustries(user.getIndustries());
-        app.setPositions(user.getPositions());
-        app.setKeyCompetencies(user.getKeyCompetencies());
-        repository.save(app);
-        return "Applicant is created in the database";
-    }
+  /**
+   * @param applicant - JSON request's user object
+   *     <p>save the user "Applicant" in the DB using repository
+   * @return "Applicant is created in the database"
+   */
+  @PostMapping("/createApplicant")
+  public String createApplicant(@RequestBody ApplicantUI applicant) {
+    Repository myRepos = new Repository(repository); // todo singleton pattern
+    myRepos.storeApplicantOnDB(applicant);
+    return "Applicant is created in the database";
+  }
 
-    @PostMapping("/createAdmin")
-    public String createAdmin(@RequestBody UserUI userUI) {
-        repository.save(new Admin(userUI.getFirstName(), userUI.getLastName()));
-        return "Admin is created in the database";
-    }
+  @PostMapping("/createAdmin")
+  public String createAdmin(@RequestBody UserUI userUI) {
+    repository.save(new Admin(userUI.getFirstName(), userUI.getLastName()));
+    return "Admin is created in the database";
+  }
 }
