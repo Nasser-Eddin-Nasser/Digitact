3618701d4e580293b559443bc2a510dce761d11e
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-06-11 14:45:05 +0200

    implement Image classes

--- Backend/src/main/java/Digitact/Backend/Model/Image/Block.java
+++ Backend/src/main/java/Digitact/Backend/Model/Image/Block.java
@@ -1,4 +1,60 @@
 package Digitact.Backend.Model.Image;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.UUID;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "blocks")
 public class Block {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long sqlId;
+
+    private String id;
+
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "imageId")
+    private AppImage appImage;
+
+    @Column(name = "series")
+    private int series;
+
+    public Block() {}
+
+    @JsonIgnore
+    public Block(AppImage appImage, int series) {
+        this.series = series;
+        this.id = UUID.randomUUID().toString();
+        this.appImage = appImage;
+    }
+
+    public void createId() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public AppImage getAppImage() {
+        return appImage;
+    }
+
+    public void setAppImage(AppImage appImage) {
+        this.appImage = appImage;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
 }
--- Backend/src/main/java/Digitact/Backend/Model/Image/ImageString.java
+++ Backend/src/main/java/Digitact/Backend/Model/Image/ImageString.java
@@ -1,4 +1,28 @@
 package Digitact.Backend.Model.Image;
 
 public class ImageString {
+    private String content;
+
+    private ImageType type;
+
+    public ImageString(String content, ImageType type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ImageType getType() {
+        return type;
+    }
+
+    public void setType(ImageType type) {
+        this.type = type;
+    }
 }
