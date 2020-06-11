389f5574cfcca5018269eb99a4397d38215ed714
MBuchalik <dev@martin-buchalik.de>
2020-06-11 17:50:43 +0200

    Add basic document photo taking functionality

--- frontend/src/app/forms/steps/documents/documents.component.ts
+++ frontend/src/app/forms/steps/documents/documents.component.ts
@@ -3,8 +3,15 @@
     This component renders the documents step view and its actions.
 */
 import { Component, Input } from '@angular/core';
+import {
+  CameraOptions,
+  CameraPhoto,
+  CameraResultType,
+  CameraSource,
+  Plugins,
+} from '@capacitor/core';
 
-import { FormGroup } from '../../../common/forms/forms';
+import { FormControl, FormGroup } from '../../../common/forms/forms';
 import { FormsData } from '../../../model/forms-data.model';
 
 @Component({
@@ -18,4 +25,30 @@ export class DocumentsComponent {
    */
   @Input()
   formsData: FormGroup<FormsData>;
+
+  takePicture(): void {
+    const cameraConfig: CameraOptions = {
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera,
+    };
+    Plugins.Camera.getPhoto(cameraConfig)
+      .then((result) => {
+        this.handleTakenPicture(result);
+      })
+      .catch(() => {
+        // It looks like closing the Camera overlay in the webview, rejects the promise
+      });
+  }
+
+  private handleTakenPicture(picture: CameraPhoto): void {
+    const base64Photo = 'data:image/jpeg;base64,' + picture.base64String;
+
+    const newFormControl = new FormControl<string>(base64Photo);
+
+    this.formsData.controls.documents.controls.documentsBase64.push(
+      newFormControl
+    );
+  }
 }
--- frontend/src/app/forms/steps/documents/documents.module.ts
+++ frontend/src/app/forms/steps/documents/documents.module.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 import { DocumentsComponent } from './documents.component';
 
 @NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule],
   declarations: [DocumentsComponent],
   exports: [DocumentsComponent],
 })
