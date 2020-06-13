78bc998907191e10c23d59f167b481dbfa2551e5
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-06-13 10:39:24 +0200

    move config info to  ConfigProperties class

--- Backend/src/main/java/Digitact/Backend/ConfigProperties.java
+++ Backend/src/main/java/Digitact/Backend/ConfigProperties.java
@@ -1,11 +1,11 @@
-package Digitact.Backend.Model;
+package Digitact.Backend;
 
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigProperties {
-  public static final String absoluteFileSystemPath =
-      "\\\\LENOVO-PC\\Users\\EDDIN\\Desktop\\SS20\\AMOS\\FileSystem\\";
-  public static final String blockFormat = ".txt";
-  public static final long BLOCKSIZE = 1024 * 1024; // 1 MiB
+    public static final String absoluteFileSystemPath = "xx\\xx\\";
+    public static final String BLOCKFORMAT = ".txt";
+    public static final long BLOCKSIZE = 1024 * 1024; // 1 MiB
+    public static final int Max_Repetition_Try = 5;
 }
