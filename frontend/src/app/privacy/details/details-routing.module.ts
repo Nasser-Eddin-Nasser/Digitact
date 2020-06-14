d2aa302586953062a61441aee17bda6a47aba800
Bharathwaj-Ravi-1995 <bharathravi1995@gmail.com>
2020-06-14 22:35:01 +0530

    Policy details page created

--- frontend/src/app/privacy/details/details-routing.module.ts
+++ frontend/src/app/privacy/details/details-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PrivacyPage } from './privacy.page';
+import { DetailsPage } from './details.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: PrivacyPage
-  },
-  {
-    path: 'details',
-    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+    component: DetailsPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class PrivacyPageRoutingModule {}
+export class DetailsPageRoutingModule {}
