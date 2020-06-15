591445e7421c5c453af9be2bdc8f46a6ed969cc1
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-06-15 20:31:31 +0200

    change gitignore to send fix build

---
+++ HR-Monitor/src/Main/Configuration.java
@@ -0,0 +1,6 @@
+package Main;
+
+public class Configuration {
+    public static final String BES_URI = "http://localhost:9090/api/HRController/";
+    public static final String absoluteFileSystemPath ="xxx";
+}
