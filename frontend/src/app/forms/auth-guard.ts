3b60fbed553b2a2e0fe3b5b26ec8e3ccc24080ed
denoribi <denis.amberg@gmail.com>
2020-06-15 12:22:00 +0200

    Make image viewer compatible with the Guard

--- frontend/src/app/forms/auth-guard.ts
+++ frontend/src/app/forms/auth-guard.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanDeactivate } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivate,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { FormsPage } from './forms.page';
+import { FULL_IMAGE_VIEWER_PATH } from './router-constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanDeactivate<FormsPage> {
-  canDeactivate(component: FormsPage): Observable<boolean> | boolean {
+  canDeactivate(
+    component: FormsPage,
+    _currentRoute: ActivatedRouteSnapshot,
+    _currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot
+  ): Observable<boolean> | boolean {
+    if (nextState?.url === FULL_IMAGE_VIEWER_PATH) {
+      return true;
+    }
     return component.mayLeaveView();
   }
 }
