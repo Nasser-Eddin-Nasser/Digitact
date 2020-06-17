2369b6e9645109eb9bd09b4950d43fb4a8247415
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-06-17 20:16:25 +0200

    still in doing AAC and AcModel

--- HR-Monitor/src/Model/MVC/AcModel.java
+++ HR-Monitor/src/Model/MVC/AcModel.java
@@ -1,7 +1,19 @@
 package Model.MVC;
 
 public class AcModel {
+    public static boolean isPasswordValid(String text) {
+        return true;
+    }
+
     public boolean checkAuthentication(String userName, String password) {
         return userName.equals("admin") && password.equals("123");
+    }
+
+    public void createNewAccount(String userName, String password, String firstName, String lastName, String adminID, String passHint, String email) {
+        System.out.println(userName + " - " + password + " - " + adminID + " - " + firstName + " - " + lastName + " - " + passHint + " - " + email);
+    }
+
+    public boolean isUserNameValid(String newUserName) { //no duplication
+        return true;
     }
 }
