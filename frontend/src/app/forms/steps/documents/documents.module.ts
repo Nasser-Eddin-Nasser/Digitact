bfd77bfd69252c2e152ac3559a5ba369da1197b7
MBuchalik <dev@martin-buchalik.de>
2020-06-19 12:59:43 +0200

    Improve image loading on the documents overview page

--- frontend/src/app/forms/steps/documents/documents.module.ts
+++ frontend/src/app/forms/steps/documents/documents.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
+import { FormsCommonModule } from '../../common/forms-common.module';
+
 import { DocumentsComponent } from './documents.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    FormsCommonModule,
+  ],
   declarations: [DocumentsComponent],
   exports: [DocumentsComponent],
 })
