9d9a5f842a72af662afadf1477ba2d1870ed2608
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-06-19 10:17:47 +0200

    create adminUI class for json and implement basic methodes

--- Backend/src/main/java/Digitact/Backend/Model/User/AdminUI.java
+++ Backend/src/main/java/Digitact/Backend/Model/User/AdminUI.java
@@ -1,4 +1,54 @@
 package Digitact.Backend.Model.User;
 
-public class AdminUI {
+public class AdminUI implements IUser {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String passHint;
+    private String password; // todo
+
+    protected AdminUI() {}
+
+    public AdminUI(
+            String firstName,
+            String lastName,
+            String userName,
+            String email,
+            String passHint,
+            String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.passHint = passHint;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassHint() {
+        return passHint;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
 }
