a53939f4ece26767829259333599c73cf40ad7e6
Bharathwaj-Ravi-1995 <bharathravi1995@gmail.com>
2020-06-20 12:15:00 +0530

    Eductaion Info step strings added to I18N

--- frontend/src/app/forms/steps/education-info-entry/education-info-entry.component.ts
+++ frontend/src/app/forms/steps/education-info-entry/education-info-entry.component.ts
@@ -7,19 +7,31 @@ import { ModalController } from '@ionic/angular';
 
 import { FormGroup } from '../../../common/forms/forms';
 import { EducationInfoEntry } from '../../../model/forms-data.model';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'forms-education-info-entry',
   templateUrl: './education-info-entry.component.html',
   styleUrls: ['./education-info-entry.component.scss'],
 })
 export class EducationInfoEntryComponent {
-  constructor(public modalController: ModalController) {}
+  constructor(
+    public modalController: ModalController,
+    private translate: TranslateService
+  ) {}
 
   degreeTypeArray = [
-    { value: 'Master', displayName: 'Master' },
-    { value: 'Bachelor', displayName: 'Bachelor' },
-    { value: 'School', displayName: 'School' },
+    {
+      value: 'Master',
+      displayName: this.translate.instant('educationInfo.master'),
+    },
+    {
+      value: 'Bachelor',
+      displayName: this.translate.instant('educationInfo.bachelor'),
+    },
+    {
+      value: 'School',
+      displayName: this.translate.instant('educationInfo.school'),
+    },
   ];
 
   @Input() education: FormGroup<EducationInfoEntry>;
