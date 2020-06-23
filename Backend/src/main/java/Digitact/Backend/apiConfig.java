83436887f80b708c79f51839daa2d5b2b70f960f
vishwasanavatti <vishwasanavatti@gmail.com>
2020-06-23 04:14:47 +0200

    Logout and refresh function added

--- Backend/src/main/java/Digitact/Backend/apiConfig.java
+++ Backend/src/main/java/Digitact/Backend/apiConfig.java
@@ -18,7 +18,7 @@ public class apiConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8100")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST")
                         .allowCredentials(false)
                         .maxAge(3600);
