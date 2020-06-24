4c1a79addc3d88bf13d072e6cdf5103255b6d0b6
vishwasanavatti <vishwasanavatti@gmail.com>
2020-06-24 21:33:10 +0200

    work experience page created

---
+++ frontend/src/app/forms/steps/work-experience/work-experience.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experience',
+  templateUrl: './work-experience.component.html',
+  styleUrls: ['./work-experience.component.scss'],
+})
+export class WorkExperienceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
