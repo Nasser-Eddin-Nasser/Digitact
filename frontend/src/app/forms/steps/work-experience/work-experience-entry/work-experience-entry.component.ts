dbc0f351a333abd00b0d95b2e89977eada44fe9c
vishwasanavatti <vishwasanavatti@gmail.com>
2020-06-25 00:50:21 +0200

    page look and work experience entry component added

---
+++ frontend/src/app/forms/steps/work-experience/work-experience-entry/work-experience-entry.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experience-entry',
+  templateUrl: './work-experience-entry.component.html',
+  styleUrls: ['./work-experience-entry.component.scss'],
+})
+export class WorkExperienceEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- frontend/src/app/forms/steps/work-experience/work-experience.component.ts
+++ frontend/src/app/forms/steps/work-experience/work-experience.component.ts
@@ -1,7 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 
-import { FormGroup } from '../../../common/forms/forms';
-import { FormsData } from '../../../model/forms-data.model';
+import { FormControl, FormGroup } from '../../../common/forms/forms';
+import {
+  FormsData,
+  WorkExperienceEntry,
+} from '../../../model/forms-data.model';
+
+import { WorkExperienceEntryComponent } from './work-experience-entry/work-experience-entry.component';
 
 @Component({
   selector: 'form-work-experience',
@@ -11,5 +18,44 @@ import { FormsData } from '../../../model/forms-data.model';
 export class WorkExperienceComponent {
   @Input()
   formsData: FormGroup<FormsData>;
-  constructor() {}
+  constructor(public modalController: ModalController) {}
+
+  /**
+   * add education info entry
+   */
+  async addWorkExperience(): Promise<void> {
+    const addWorkExp = new FormGroup<WorkExperienceEntry>({
+      jobTitle: new FormControl('', Validators.required),
+      company: new FormControl('', Validators.required),
+      employmentType: new FormControl('', Validators.required),
+      startDate: new FormControl('', Validators.required),
+      endDate: new FormControl(''),
+      description: new FormControl(''),
+    });
+    const modal = await this.modalController.create({
+      component: WorkExperienceEntryComponent,
+      componentProps: {
+        work: addWorkExp,
+      },
+      swipeToClose: true,
+      presentingElement: await this.modalController.getTop(),
+    });
+    /**
+     * save and cancel of education info form
+     */
+    modal.onDidDismiss().then(() => {
+      this.formsData.controls.workExperienceInfo.controls.workExperienceForm.push(
+        addWorkExp
+      );
+    });
+    return await modal.present();
+  }
+
+  modifyWorkExperience(): void {}
+
+  deleteWorkExperience(index: number): void {
+    this.formsData.controls.educationInfo.controls.educationInfoForm.removeAt(
+      index
+    );
+  }
 }
