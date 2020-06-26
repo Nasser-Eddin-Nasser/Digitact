1d9753ed80685c6e5499381451d95641e523862e
Bharathwaj-Ravi-1995 <bharathravi1995@gmail.com>
2020-06-26 19:57:51 +0530

    Employment type enum added

---
+++ Backend/src/main/java/Digitact/Backend/Model/EmploymentType.java
@@ -0,0 +1,11 @@
+package Digitact.Backend.Model;
+
+public enum EmploymentType {
+    Full-Time,
+    Part-Time,
+    Self-Employed,
+    Freelance,
+    Contract,
+    Internship,
+    Apprenticeship
+}
