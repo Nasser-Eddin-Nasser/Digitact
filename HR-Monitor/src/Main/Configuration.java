bd33b103ad6cb049feb12846dc06db2f89cc1b01
dalex135 <don6alex135@gmail.com>
2020-06-27 21:39:01 +0200

    HR Monitor Configurations

--- HR-Monitor/src/Main/Configuration.java
+++ HR-Monitor/src/Main/Configuration.java
@@ -1,6 +1,9 @@
 package Main;
 
+import java.nio.file.Paths;
+
 public class Configuration {
     public static final String BES_URI = "http://localhost:9090/api/HRController/";
-    public static final String absoluteFileSystemPath = System.getProperty("user.dir")+"\\FileSystem\\";
+    public static final String absoluteFileSystemPath =
+            Paths.get(System.getProperty("user.dir")).getParent().toAbsolutePath()+"\\FileSystem\\";
 }
