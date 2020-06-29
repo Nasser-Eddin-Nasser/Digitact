cc573bddf55e8d4b271eedc340329bd727134113
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-06-29 15:58:30 +0200

    fine extend education test

--- Backend/src/test/java/Digitact/Backend/Model/EducationTest.java
+++ Backend/src/test/java/Digitact/Backend/Model/EducationTest.java
@@ -1,7 +1,11 @@
 package Digitact.Backend.Model;
 
+import Digitact.Backend.Model.User.Applicant;
+import Digitact.Backend.Model.User.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertFalse;
 
 class EducationTest {
 
@@ -9,5 +13,9 @@ class EducationTest {
     void createEducation() {
         Education education = new Education();
         Assertions.assertThat(education).isNotNull();
+        User user = new Applicant("fn", "ln");
+        education.setUser(user);
+        Assertions.assertThat(education.getUser().getClass().equals(Applicant.class));
+        assertFalse(education.getUser().getClass().equals(User.class));
     }
 }
