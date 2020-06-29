f8d0da0971e38bf779425de93d6443fdf7fd8382
vishwasanavatti <vishwasanavatti@gmail.com>
2020-06-29 18:26:49 +0200

    HTTP implementation changed

--- frontend/src/app/applicants/applications-upload/applications-upload.page.ts
+++ frontend/src/app/applicants/applications-upload/applications-upload.page.ts
@@ -30,9 +30,9 @@ export class ApplicationsUploadPage implements OnInit {
    */
   totalSize = 0;
   /**
-   * The application size that is being uploaded.
+   * The application count that is being uploaded.
    */
-  uploadSize = 1;
+  uploadingCount = 1;
   /**
    * To check whether all the applications are uploaded.
    */
@@ -49,6 +49,14 @@ export class ApplicationsUploadPage implements OnInit {
    * URL of the server host.
    */
   url = 'http://localhost:9090';
+  /**
+   * refers to the application that is being processed
+   */
+  index = 0;
+  /**
+   * To store all the application from the local database
+   */
+  applicantDetailsList: FormValue<FormsData>[];
 
   /**
    * In this method locally stored data are fetched and post request function is called for one application at time.
@@ -57,147 +65,146 @@ export class ApplicationsUploadPage implements OnInit {
     this.storage
       .getAllItems<FormValue<FormsData>>(this.storage.applicantDetailsDb)
       .then((data) => {
-        const applicantDetailsList = data.filter((x) => x.isRated === 1);
-        this.totalSize = applicantDetailsList.length;
-        for (const applicantData of applicantDetailsList) {
-          this.storage
-            .getItem<FormValue<RatingForm>>(
-              this.storage.applicantRatingsDb,
-              applicantData.id
-            )
-            .then(async (applicantHrScore) => {
-              if (!this.isError) {
-                await this.sendPostRequest(applicantData, applicantHrScore);
-              }
-            });
-        }
+        this.applicantDetailsList = data.filter((x) => x.isRated === 1);
+        this.totalSize = this.applicantDetailsList.length;
+        this.sendPostRequest();
       });
   }
 
-  /**
-   * In this method timeout of 1 second is speified to have a better readability of the application upload.
-   */
-  private sleep(ms: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
   /**
    * In this method a post request is made to the server.
    */
+  private sendPostRequest(): void {
+    const applicantData = this.applicantDetailsList[this.index];
+    /**
+     * In this method locally stored data HR rating data is fetched and set according to JSON format
+     */
+    this.storage
+      .getItem<FormValue<RatingForm>>(
+        this.storage.applicantRatingsDb,
+        applicantData.id
+      )
+      .then((val) => {
+        const applicantHRScore = {
+          rhetoric: val.applicantScore.rhetoric,
+          motivation: val.applicantScore.motivation,
+          selfAssurance: val.applicantScore.selfAssurance,
+          personalImpression: val.applicantScore.personalImpression,
+          impression: val.impressionInfo.impression,
+        };
 
-  private sendPostRequest(
-    inp: FormValue<FormsData>,
-    hrInp: FormValue<RatingForm>
-  ): Promise<void> {
-    console.log(hrInp);
-    // const hr = { ...hrInp.applicantScore };
-    const hr = {
-      rhetoric: hrInp.applicantScore.rhetoric,
-      motivation: hrInp.applicantScore.motivation,
-      selfAssurance: hrInp.applicantScore.selfAssurance,
-      personalImpression: hrInp.applicantScore.personalImpression,
-      impression: hrInp.impressionInfo.impression,
-    };
+        /*
+        TODO: The values are all optional.
+        So, we should make sure that, if a value doesn't exist, this is handled appropriately.
+        This doesn't have to be done for all entries (e.g. the first name always exists,even though it is typed here as an optional thing).
+        But we should at least make sure that we have covered all truly optional ones.
+      */
+        const keyCompetence: Array<{
+          category: string;
+          name: string;
+          rating: number;
+        }> = [];
 
-    /*
-       TODO: The values are all optional.
-       So, we should make sure that, if a value doesn't exist, this is handled appropriately.
-       This doesn't have to be done for all entries (e.g. the first name always exists, even though it is typed here as an optional thing).
-       But we should at least make sure that we have covered all truly optional ones.
-    */
-    /*
-      TODO: The "HR part" (so the rating of the applicant) is currently not getting sent to the server.
-    */
-    return this.sleep(1000).then(() => {
-      const keyCompetence: Array<{
-        category: string;
-        name: string;
-        rating: number;
-      }> = [];
-
-      const images: Array<{ content: string; type: string }> = [];
-      /**
-       * key competencies are mapped according json format required by server.
-       */
-      Object.entries(inp.keyCompetencies).map(([k, v]) => {
-        v.forEach((x: KeyCompetenciesEntry) => {
-          keyCompetence.push({ category: k, name: x.name, rating: x.rating });
+        const images: Array<{ content: string; type: string }> = [];
+        /**
+         * key competencies are mapped according json format required by server.
+         */
+        Object.entries(applicantData.keyCompetencies).map(([k, v]) => {
+          v.forEach((x: KeyCompetenciesEntry) => {
+            keyCompetence.push({ category: k, name: x.name, rating: x.rating });
+          });
         });
-      });
-      /**
-       * picture of applicant and documents is mapped according json format required by server.
-       */
-      images.push({
-        content: inp.profilePicture.pictureBase64,
-        type: 'profilePic',
-      });
-      inp.documents.documentsBase64.forEach((x) => {
-        images.push({ content: x, type: 'CV' });
-      });
-      /**
-       * complete json structure is formed here to send to server.
-       */
-      const formsData = {
-        firstName: inp.basicInfo.firstName,
-        lastName: inp.basicInfo.lastName,
-        phone: inp.contactInfo.phoneNumber,
-        email: inp.contactInfo.eMail,
-        title: inp.basicInfo.salutation,
-        linkedIn: inp.contactInfo.linkedIn,
-        xing: inp.contactInfo.xing,
-        imageList: images,
-        workExperiences: inp.workExperienceInfo.workExperienceForm,
-        educations: inp.educationInfo.educationInfoForm,
-        industries: inp.fieldDesignationInfo.field,
-        positions: inp.fieldDesignationInfo.designation,
-        keyCompetencies: keyCompetence,
-        additionalInfo: inp.additionalInfo.additionalInfo,
-        hrRating: hr,
-      };
+        /**
+         * picture of applicant and documents is mapped according json format required by server.
+         */
+        images.push({
+          content: applicantData.profilePicture.pictureBase64,
+          type: 'profilePic',
+        });
+        applicantData.documents.documentsBase64.forEach((x) => {
+          images.push({ content: x, type: 'CV' });
+        });
+        /**
+         * complete json structure is formed here to send to server.
+         */
+        const formsData = {
+          firstName: applicantData.basicInfo.firstName,
+          lastName: applicantData.basicInfo.lastName,
+          phone: applicantData.contactInfo.phoneNumber,
+          email: applicantData.contactInfo.eMail,
+          title: applicantData.basicInfo.salutation,
+          linkedIn: applicantData.contactInfo.linkedIn,
+          xing: applicantData.contactInfo.xing,
+          imageList: images,
+          workExperiences: applicantData.workExperienceInfo.workExperienceForm,
+          educations: applicantData.educationInfo.educationInfoForm,
+          industries: applicantData.fieldDesignationInfo.field,
+          positions: applicantData.fieldDesignationInfo.designation,
+          keyCompetencies: keyCompetence,
+          additionalInfo: applicantData.additionalInfo.additionalInfo,
+          hrRating: applicantHRScore,
+        };
 
-      const headers = new HttpHeaders();
-      headers.append('Content-Type', 'application/json');
-      /**
-       * post request is made here and based on response locally stored data is deleted
-       * error and success messages are shown wherever necessary
-       */
-      this.httpClient
-        .post(this.url + '/api/controller/createApplicant', formsData, {
-          responseType: 'text',
-          observe: 'response',
-        })
-        .subscribe(
-          (response) => {
-            if (response.status === 201) {
-              // this.storage.deleteItem(this.storage.applicantDetailsDb, inp.id);
-              // this.storage.deleteItem(this.storage.applicantRatingsDb, inp.id);
-              if (this.uploadSize === this.totalSize) {
-                this.isSuccess = true;
-                this.completionAlert();
+        const headers = new HttpHeaders();
+        headers.append('Content-Type', 'application/json');
+        /**
+         * post request is made here and based on response locally stored data is deleted
+         * error and success messages are shown wherever necessary
+         */
+        this.httpClient
+          .post(this.url + '/api/controller/createApplicant', formsData, {
+            responseType: 'text',
+            observe: 'response',
+          })
+          .subscribe(
+            (response) => {
+              if (response.status === 201) {
+                this.storage.deleteItem(
+                  this.storage.applicantDetailsDb,
+                  applicantData.id
+                );
+                this.storage.deleteItem(
+                  this.storage.applicantRatingsDb,
+                  applicantData.id
+                );
+                this.index++;
+                if (this.uploadingCount === this.totalSize) {
+                  this.isSuccess = true;
+                  this.completionAlert();
+                } else {
+                  this.uploadingCount++;
+                }
+                if (this.index <= this.totalSize - 1) {
+                  this.sendPostRequest();
+                }
+              }
+            },
+            (error) => {
+              this.index++;
+              this.isError = true;
+              if (error.status === 500) {
+                this.errorMessage = this.translate.instant(
+                  'applicantsUploadPage.nApplicantsFailedToUploadMessage',
+                  {
+                    failedCount: this.totalSize - this.uploadingCount + 1,
+                    errorMessage: error.error,
+                  }
+                );
               } else {
-                this.uploadSize++;
+                this.errorMessage = this.translate.instant(
+                  'applicantsUploadPage.serverConnectionErrorMessage'
+                );
+              }
+              if (this.index <= this.totalSize - 1) {
+                this.sendPostRequest();
               }
             }
-          },
-          (error) => {
-            this.isError = true;
-            if (error.status === 500) {
-              this.errorMessage = this.translate.instant(
-                'applicantsUploadPage.nApplicantsFailedToUploadMessage',
-                {
-                  failedCount: this.totalSize - this.uploadSize + 1,
-                  errorMessage: error.error,
-                }
-              );
-            } else {
-              this.errorMessage = this.translate.instant(
-                'applicantsUploadPage.serverConnectionErrorMessage'
-              );
-            }
-          }
-        );
-    });
+          );
+      });
   }
-
+  /**
+   * In this method completion notification is displayed
+   */
   async completionAlert(): Promise<void> {
     const toast = await this.toastController.create({
       message: this.translate.instant(
