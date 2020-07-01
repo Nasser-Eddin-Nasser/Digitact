b6e1de78ae323621a6ca1885076eebfdadacef47
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-01 15:50:01 +0200

    refactor: remove wrong path

--- HR-Monitor/src/Main/Configuration.java
+++ HR-Monitor/src/Main/Configuration.java
@@ -1,10 +1,7 @@
 package Main;
 
-import java.nio.file.Paths;
 
 public class Configuration {
     public static final String BES_URI = "http://localhost:9090/api/HRController/";
-    public static final String absoluteFileSystemPath =
-            Paths.get(System.getProperty("user.dir")).getParent().toAbsolutePath()
-                    + "\\FileSystem\\";
+    public static final String absoluteFileSystemPath ="xx";
 }
