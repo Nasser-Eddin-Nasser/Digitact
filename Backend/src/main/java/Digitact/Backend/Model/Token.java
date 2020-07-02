ecf486d86a5ac05e110b2d5414d611e2658be337
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-02 19:21:24 +0200

    implement BES token

--- Backend/src/main/java/Digitact/Backend/Model/Token.java
+++ Backend/src/main/java/Digitact/Backend/Model/Token.java
@@ -1,4 +1,30 @@
 package Digitact.Backend.Model;
 
+import Digitact.Backend.Model.User.Admin;
+
 public class Token {
+  private Long uniqueRandom;
+  private String clientURL;
+  private Admin loggedinAdmin;
+
+  public Token(Long uniqueRandom, String clientURL) {
+    this.uniqueRandom = uniqueRandom;
+    this.clientURL = clientURL;
+  }
+
+  public Admin getLoggedinAdmin() {
+    return loggedinAdmin;
+  }
+
+  public void setLoggedinAdmin(Admin loggedinAdmin) {
+    this.loggedinAdmin = loggedinAdmin;
+  }
+
+  public Long getUniqueRandom() {
+    return uniqueRandom;
+  }
+
+  public String getClientURL() {
+    return clientURL;
+  }
 }
