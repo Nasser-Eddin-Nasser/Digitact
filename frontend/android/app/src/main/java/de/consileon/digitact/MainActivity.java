ca621bba28b7a94d854cc9dace3a4ec055311f11
MBuchalik <dev@martin-buchalik.de>
2020-07-02 19:09:18 +0200

    Make it possible to use Dark Mode on Android

--- frontend/android/app/src/main/java/de/consileon/digitact/MainActivity.java
+++ frontend/android/app/src/main/java/de/consileon/digitact/MainActivity.java
@@ -1,6 +1,10 @@
 package de.consileon.digitact;
 
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -17,5 +21,39 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+
+    handleDisabledOrEnabledDarkMode();
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+
+    handleDisabledOrEnabledDarkMode();
+  }
+
+  /**
+   * If Dark Mode is currently turned on, set it for our WebView.
+   * If not, disable Dark Mode for the WebView.
+   */
+  private void handleDisabledOrEnabledDarkMode() {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+      return;
+    }
+
+    WebView webView = this.bridge.getWebView();
+    WebSettings webSettings = webView.getSettings();
+
+    if ((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_YES) == 0) {
+      webSettings.setForceDark(WebSettings.FORCE_DARK_OFF);
+      return;
+    }
+
+    webSettings.setForceDark(WebSettings.FORCE_DARK_ON);
   }
 }
