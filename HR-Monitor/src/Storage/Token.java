13ae5115606d725b07d2366a3f2120be09c6bd22
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-02 19:22:02 +0200

    implement MHR token

--- HR-Monitor/src/Storage/Token.java
+++ HR-Monitor/src/Storage/Token.java
@@ -1,4 +1,18 @@
 package Storage;
 
+import Model.User.Admin;
+
 public class Token {
+    private final Long besNumber;
+    private Admin loggedinAdmin;
+
+    public Token(Long besNumber) {
+        this.besNumber = besNumber;
+    }
+
+    public void setLoggedinAdmin(Admin loggedinAdmin) {
+        this.loggedinAdmin = loggedinAdmin;
+    }
+
+
 }
