105d237364db48088a12a3686199b657d9e5c5e7
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-02 19:25:18 +0200

    add token to the Repository

--- Backend/src/main/java/Digitact/Backend/Storage/Repository.java
+++ Backend/src/main/java/Digitact/Backend/Storage/Repository.java
@@ -1,97 +1,121 @@
 package Digitact.Backend.Storage;
 
 import Digitact.Backend.Exception.ImageException;
-import Digitact.Backend.Model.Education;
-import Digitact.Backend.Model.HrRating;
+import Digitact.Backend.Model.*;
 import Digitact.Backend.Model.Image.AppImage;
 import Digitact.Backend.Model.Image.ImageString;
-import Digitact.Backend.Model.KeyCompetence;
 import Digitact.Backend.Model.User.Applicant;
 import Digitact.Backend.Model.User.ApplicantUI;
-import Digitact.Backend.Model.WorkExperience;
 import Digitact.Backend.Util.ImageTools;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
 public class Repository {
-    IDataRepository repo;
+  IDataRepository repo;
+  private static List<Token> tokenList;
 
-    @Autowired
-    public Repository(IDataRepository repo) {
-        if (this.repo == null) this.repo = repo;
-    }
+  @Autowired
+  public Repository(IDataRepository repo) {
+    if (this.repo == null) this.repo = repo;
+  }
 
-    public boolean storeApplicantOnDB(ApplicantUI applicant) {
-        boolean isImageSuccessfullyStored = true;
-        Applicant app = new Applicant(applicant.getFirstName(), applicant.getLastName());
-        app.setEmail(applicant.getEmail());
-        app.setPhone(applicant.getPhone());
-        addEducationInfoToApplicant(applicant.getEducations(), app);
-        addWorkExperienceToApplicant(applicant.getWorkExperiences(), app);
-        addKeyCompetencesToApplicant(applicant.getKeyCompetencies(), app);
-        addHrRatingToApplicant(applicant.getHrRating(), app);
-        app.setIndustries(applicant.getIndustries());
-        app.setPositions(applicant.getPositions());
-        app.setLinkedIn(applicant.getLinkedIn());
-        app.setXing(applicant.getXing());
-        app.setTitle(applicant.getTitle());
-        app.setAdditionalInfo(applicant.getAdditionalInfo());
-        boolean hasImages = applicant.getImageList() != null;
-        if (hasImages)
-            isImageSuccessfullyStored = addImagesToApplicant(applicant.getImageList(), app);
-        if (isImageSuccessfullyStored) {
-            try {
-                repo.save(app);
-            } catch (Exception e) {
-                return false;
-            }
-        }
-        return (hasImages && isImageSuccessfullyStored)
-                || (!hasImages && isImageSuccessfullyStored);
-    }
+  public static List<Token> getTokenList() {
+    if (tokenList == null) new ArrayList<Token>();
+    return tokenList;
+  }
 
-    private boolean addImagesToApplicant(List<ImageString> imageList, Applicant app) {
-        AtomicBoolean isSuccessful = new AtomicBoolean(true);
-        imageList.forEach(
-                x -> {
-                    AppImage img = null;
-                    try {
-                        img = ImageTools.createAppImage(x.getContent(), x.getType());
-                    } catch (ImageException e) {
-                        isSuccessful.set(false);
-                        return;
-                    }
-                    app.addImage(img);
-                    img.setUser(app);
-                });
-        return isSuccessful.get();
-    }
+  public static boolean checkTokenValidation(Token token) {
+    if (tokenList != null) new ArrayList<Token>();
+    return tokenList.stream()
+            .filter(
+                x ->
+                    x.getClientURL().equals(token.getClientURL())
+                        && x.getUniqueRandom().equals(token.getUniqueRandom())
+                        && x.getLoggedinAdmin()
+                            .getUserName()
+                            .equals(token.getLoggedinAdmin().getUserName()))
+            .collect(Collectors.toList())
+            .size()
+        > 0;
+  }
 
-    private void addKeyCompetencesToApplicant(List<KeyCompetence> keyCompetences, Applicant app) {
-        if (keyCompetences != null) {
-            keyCompetences.forEach(x -> x.setUser(app));
-            app.setKeyCompetencies(keyCompetences);
-        }
-    }
+  public static void insertTokenToTokenList(Token token) {
+    if (tokenList == null) new ArrayList<Token>();
+    tokenList.add(token);
+  }
 
-    private void addEducationInfoToApplicant(List<Education> educationList, Applicant app) {
-        if (educationList != null) {
-            educationList.forEach(x -> x.setUser(app));
-            app.setEducations(educationList);
-        }
+  public boolean storeApplicantOnDB(ApplicantUI applicant) {
+    boolean isImageSuccessfullyStored = true;
+    Applicant app = new Applicant(applicant.getFirstName(), applicant.getLastName());
+    app.setEmail(applicant.getEmail());
+    app.setPhone(applicant.getPhone());
+    addEducationInfoToApplicant(applicant.getEducations(), app);
+    addWorkExperienceToApplicant(applicant.getWorkExperiences(), app);
+    addKeyCompetencesToApplicant(applicant.getKeyCompetencies(), app);
+    addHrRatingToApplicant(applicant.getHrRating(), app);
+    app.setIndustries(applicant.getIndustries());
+    app.setPositions(applicant.getPositions());
+    app.setLinkedIn(applicant.getLinkedIn());
+    app.setXing(applicant.getXing());
+    app.setTitle(applicant.getTitle());
+    app.setAdditionalInfo(applicant.getAdditionalInfo());
+    boolean hasImages = applicant.getImageList() != null;
+    if (hasImages) isImageSuccessfullyStored = addImagesToApplicant(applicant.getImageList(), app);
+    if (isImageSuccessfullyStored) {
+      try {
+        repo.save(app);
+      } catch (Exception e) {
+        return false;
+      }
     }
+    return (hasImages && isImageSuccessfullyStored) || (!hasImages && isImageSuccessfullyStored);
+  }
 
-    private void addWorkExperienceToApplicant(
-            List<WorkExperience> workExperienceList, Applicant app) {
-        if (workExperienceList != null) {
-            workExperienceList.forEach(x -> x.setUser(app));
-            app.setWorkExperiences(workExperienceList);
-        }
-    }
+  private boolean addImagesToApplicant(List<ImageString> imageList, Applicant app) {
+    AtomicBoolean isSuccessful = new AtomicBoolean(true);
+    imageList.forEach(
+        x -> {
+          AppImage img = null;
+          try {
+            img = ImageTools.createAppImage(x.getContent(), x.getType());
+          } catch (ImageException e) {
+            isSuccessful.set(false);
+            return;
+          }
+          app.addImage(img);
+          img.setUser(app);
+        });
+    return isSuccessful.get();
+  }
 
-    private void addHrRatingToApplicant(HrRating hrRating, Applicant app) {
-        hrRating.setUser(app);
-        app.setHrRating(hrRating);
+  private void addKeyCompetencesToApplicant(List<KeyCompetence> keyCompetences, Applicant app) {
+    if (keyCompetences != null) {
+      keyCompetences.forEach(x -> x.setUser(app));
+      app.setKeyCompetencies(keyCompetences);
     }
+  }
+
+  private void addEducationInfoToApplicant(List<Education> educationList, Applicant app) {
+    if (educationList != null) {
+      educationList.forEach(x -> x.setUser(app));
+      app.setEducations(educationList);
+    }
+  }
+
+  private void addWorkExperienceToApplicant(
+      List<WorkExperience> workExperienceList, Applicant app) {
+    if (workExperienceList != null) {
+      workExperienceList.forEach(x -> x.setUser(app));
+      app.setWorkExperiences(workExperienceList);
+    }
+  }
+
+  private void addHrRatingToApplicant(HrRating hrRating, Applicant app) {
+    hrRating.setUser(app);
+    app.setHrRating(hrRating);
+  }
 }
