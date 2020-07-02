511a7235db20c042c723371e0810fd4312b939c1
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-02 09:12:37 +0200

    add menu items to the enum

--- HR-Monitor/src/Model/MenuItem.java
+++ HR-Monitor/src/Model/MenuItem.java
@@ -1,4 +1,25 @@
 package Model;
 
 public enum MenuItem {
+    Applicants("Applicants"),
+    CreateAccount("Create Account"),
+    Logout("Logout");
+    private String menuItem;
+
+    MenuItem(String menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public String getMenuItem() {
+        return menuItem;
+    }
+
+    public static MenuItem fromString(String text) {
+        for (MenuItem menuItem : MenuItem.values()) {
+            if (menuItem.menuItem.equalsIgnoreCase(text)) {
+                return menuItem;
+            }
+        }
+        return null;
+    }
 }
