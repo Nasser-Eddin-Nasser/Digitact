11b5ef5965f8b073a38638711728f9b0796312ca
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-03 13:05:17 +0200

    show create first account windows

--- HR-Monitor/src/Controller/AcController.java
+++ HR-Monitor/src/Controller/AcController.java
@@ -4,7 +4,6 @@ import Database.Connector;
 import Database.Method;
 import Model.MVC.AcModel;
 import Storage.DBStorage;
-import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,20 +14,31 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class AcController {
-    /** Initial Login Window after program start */
+    /**
+     * Initial Login Window after program start
+     */
     private Scene viewLogin;
-    /** the stage, which holds the program */
+    /**
+     * the stage, which holds the program
+     */
     private Stage stage;
-    /** boundaries of the login-view */
+    /**
+     * boundaries of the login-view
+     */
     private double viewLoginHeight;
 
     private double viewLoginWidth;
 
-    @FXML private Button login;
-    @FXML private PasswordField myPasswordField;
+    @FXML
+    private Button login;
+    @FXML
+    private PasswordField myPasswordField;
 
-    @FXML private TextField myUserNameTextField;
+    @FXML
+    private TextField myUserNameTextField;
     private AcModel model;
 
     public static String ADMIN_USERNAME = "";
@@ -45,10 +55,18 @@ public class AcController {
         if (DBStorage.getToken() != null) {
             DBStorage.getAdminUserNames();
             offlineMode = false;
+            setModelAndStageAndScene();
+            if (DBStorage.getAdminUserNames().size() == 0) {
+                new CreateFirstAccountController(stage, model);
+            }
         } else {
             offlineMode = true;
             System.err.println("Run in offline Mode!");
         }
+
+    }
+
+    private void setModelAndStageAndScene() throws IOException {
         model = new AcModel();
         stage = new Stage();
         setscene();
