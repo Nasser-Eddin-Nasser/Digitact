6b87b05afdb5f4d4abe39f4def183cac11e381d3
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-04 08:28:42 +0200

    clean configuration up

--- Backend/src/main/java/Digitact/Backend/ConfigProperties.java
+++ Backend/src/main/java/Digitact/Backend/ConfigProperties.java
@@ -5,14 +5,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConfigProperties {
     public static final int serverPort = 9090;
-    public static final String absoluteFileSystemPath = "";
+    public static final String absoluteFileSystemPath =
+            "";
     public static final String BLOCKFORMAT = ".txt";
     public static final long BLOCKSIZE = 1024 * 1024; // 1 MiB
     public static final int Max_Repetition_Try = 5;
+    public static final String absoluteClientURL_ng = "http://localhost:4200";
+    public static final String absoluteClientURL_ionic = "http://localhost:8100";
     // test configurations
     public static boolean testEnvironment = false; // it will be dynamically set by the test classes
     public static String testAbsoluteFileSystemPath =
             "xx\\xx\\"; // it will be dynamically set by the test classes
-    public static final String absoluteClientURL_ng = "http://localhost:4200";
-    public static final String absoluteClientURL_ionic = "http://localhost:8100";
+
 }
