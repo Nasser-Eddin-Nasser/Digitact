bafcddb71974790d7869104d08ccbc531eaa73a3
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-05 16:41:59 +0200

    Merge branch 'master' of https://github.com/Nasser-Eddin-Nasser/Digitact into dalex135-BEnHR-Applicant_Status

--- HR-Monitor/src/Database/Connector.java
+++ HR-Monitor/src/Database/Connector.java
@@ -179,15 +179,6 @@ public class Connector {
         }
     }
 
-    public static void changeStatus(long appID, Status status) {
-        try {
-            handleChangeStatus(new URL(Configuration.BES_URI + "changeStatus"), appID, status);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void handleChangeStatus(URL url, long appID, Status status) {
     public static void sendPutType(Method method, Token token) {
         switch (method) {
             case putToken:
@@ -203,6 +194,45 @@ public class Connector {
         }
     }
 
+    public static void changeStatus(long appID, Status status) {
+        try {
+            handleChangeStatus(new URL(Configuration.BES_URI + "changeStatus"), appID, status);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleChangeStatus(URL url, long appID, Status status) {
+        try {
+            URLConnection uc = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection) uc;
+            http.setRequestMethod("POST"); // PUT is another valid option
+            http.setDoOutput(true);
+            http.setRequestProperty("Content-Type", "application/json; utf-8");
+            http.setRequestProperty("Accept", "application/json");
+            String message = "{ \"appID\":" + appID + ",\"status\":\"" + status.toString() + "\"}";
+
+            try (OutputStream os = http.getOutputStream()) {
+
+                byte[] input = message.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+            try (BufferedReader br =
+                    new BufferedReader(new InputStreamReader(http.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static void handlePutTokenToAdmin(URL url, Token token) {
         BufferedReader in = null;
         try {
@@ -212,26 +242,8 @@ public class Connector {
             http.setDoOutput(true);
             http.setRequestProperty("Content-Type", "application/json; utf-8");
             http.setRequestProperty("Accept", "application/json");
-            // String message = "{ \"appID\":" + appID + ",\"status\":\"" + status.toString() + "\"}";
-
-            // try (OutputStream os = http.getOutputStream()) {
-
-            //     byte[] input = message.getBytes("utf-8");
-            //     os.write(input, 0, input.length);
-            // }
-            // try (BufferedReader br =
-            //         new BufferedReader(new InputStreamReader(http.getInputStream(), "utf-8"))) {
-            //     StringBuilder response = new StringBuilder();
-            //     String responseLine = null;
-            //     while ((responseLine = br.readLine()) != null) {
-            //         response.append(responseLine.trim());
-            //     }
-            // }
-
             try (OutputStream os = http.getOutputStream()) {
                 String reqBody = token.toString();
-                //                        token.getBesNumber() + " -//- " +
-                // token.getLoggedinAdmin().getUserName();  // Todo
                 byte[] input = reqBody.getBytes("utf-8");
                 os.write(input, 0, input.length);
                 try (BufferedReader br =
