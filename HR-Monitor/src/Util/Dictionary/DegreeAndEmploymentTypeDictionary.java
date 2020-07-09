8fe6340419d3aa78bd87d224bd9410d26d3a53b0
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-09 21:07:21 +0200

    create  Dictionaries

---
+++ HR-Monitor/src/Util/Dictionary/DegreeAndEmploymentTypeDictionary.java
@@ -0,0 +1,16 @@
+package Util.Dictionary;
+
+import java.util.Map;
+
+public class DegreeDictionary implements IDictionary {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public Map<String, String> getDictionary() {
+        return null;
+    }
+}
