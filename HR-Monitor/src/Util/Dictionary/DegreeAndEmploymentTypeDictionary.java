25d76c7e66e9c15c162e00c3df424899396a3e37
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-09 21:08:00 +0200

    implement Dictionaries

--- HR-Monitor/src/Util/Dictionary/DegreeAndEmploymentTypeDictionary.java
+++ HR-Monitor/src/Util/Dictionary/DegreeAndEmploymentTypeDictionary.java
@@ -1,16 +1,34 @@
 package Util.Dictionary;
 
 import java.util.Map;
+import java.util.TreeMap;
 
-public class DegreeDictionary implements IDictionary {
-
+public class DegreeAndEmploymentTypeDictionary implements IDictionary {
+    private Map<String, String> dictionary = new TreeMap<String, String>(); // <English, German>
+    public   DegreeAndEmploymentTypeDictionary(){
+        init();
+    }
     @Override
     public void init() {
-
+        /////////// EmploymentType
+        dictionary.put("FullTime", "Vollzeit");
+        dictionary.put("PartTime", "Teilzeit");
+        dictionary.put("SelfEmployed", "Selbstständig");
+        dictionary.put("Freelance", "Freiberuflich");
+        dictionary.put("Contract", "Vertrag");
+        dictionary.put("Internship", "Praktikum");
+        dictionary.put("Apprenticeship", "Ausbildung");
+        dictionary.put("Other", "Andere");
+        dictionary.put("present", "Zurzeit");
+////////////////// Degree
+        dictionary.put("Master", "Master");
+        dictionary.put("Bachelor", "Bachelor");
+        dictionary.put("PhD", "PhD");
+        dictionary.put("School", "Schule");
     }
 
     @Override
     public Map<String, String> getDictionary() {
-        return null;
+        return dictionary;
     }
 }
