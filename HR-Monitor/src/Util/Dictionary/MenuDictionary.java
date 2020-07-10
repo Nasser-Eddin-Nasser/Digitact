f5ae1801198b651d08aae2b2dfca58430766632a
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-10 16:57:37 +0200

    add Menu words to the Dictionary

--- HR-Monitor/src/Util/Dictionary/MenuDictionary.java
+++ HR-Monitor/src/Util/Dictionary/MenuDictionary.java
@@ -1,4 +1,26 @@
 package Util.Dictionary;
 
-public class MenuDictionary {
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MenuDictionary implements IDictionary {
+    private Map<String, String> dictionary = new TreeMap<String, String>(); // <English, German>
+
+    public MenuDictionary() {
+        init();
+    }
+
+    @Override
+    public void init() {
+        if (dictionary.size() == 0) {
+            /////////////////////// Menu
+            dictionary.put("Logout", "Abmelden");
+            dictionary.put("Applicants", "Bewerber");
+            dictionary.put("Create Account", "Konto Erstellen");
+        }
+    }
+
+    public Map<String, String> getDictionary() {
+        return dictionary;
+    }
 }
