ff1844a6fd0432fb22bdd66f3a834dbb74d0c9de
dalex135 <don6alex135@gmail.com>
2020-07-12 10:43:07 +0200

    Code for the Filter

--- HR-Monitor/src/Controller/OverviewController.java
+++ HR-Monitor/src/Controller/OverviewController.java
@@ -3,32 +3,25 @@ package Controller;
 import Model.Industries;
 import Model.MVC.OverviewModel;
 import Model.Positions;
+import Model.Status;
 import Model.User.ApplicantUI;
 import Util.Dictionary.ApplicantInfoDictionary;
 import Util.Dictionary.BasicInfoDictionary;
 import Util.Dictionary.IDictionary;
+import Util.Dictionary.PositionsAndIndustriesDictionary;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import Util.Dictionary.ApplicantInfoDictionary;
-import Util.Dictionary.IDictionary;
-import Util.Dictionary.PositionsAndIndustriesDictionary;
 import javafx.beans.property.ReadOnlyLongWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 
 public class OverviewController {
     Stage stage;
@@ -36,20 +29,24 @@ public class OverviewController {
     // Create a TableView with a list of Applicants
     @FXML TableView<ApplicantUI> userTable;
     private ObservableList<ApplicantUI> observableListTableView;
-    IDictionary biDic, aiDic;
+    IDictionary biDictionary, aiDictionary, pNiDictionary;
+
     // Overview of all Applicants
     @FXML TableColumn<ApplicantUI, Number> idCol = new TableColumn<>("id");
     @FXML TableColumn<ApplicantUI, String> firstNameCol = new TableColumn<>("First Name");
     @FXML TableColumn<ApplicantUI, String> lastNameCol = new TableColumn<>("Last Name");
     @FXML TableColumn<ApplicantUI, String> status = new TableColumn<>("Status");
 
-    @FXML MenuButton mBtnStatusFX,mBtnIndustryFX,mBtnPositionFX;
+    @FXML MenuButton mBtnStatusFX, mBtnIndustryFX, mBtnPositionFX;
 
     @FXML TextField txtNameFX;
 
-    //mBtnStatusFX,mBtnIndustryFX,mBtnPositionFX;
-    PositionsAndIndustriesDictionary pNiDictionary;
-    ApplicantInfoDictionary aIDictionary;
+    List<CheckBox> cBStatus = new ArrayList<>();
+    List<CustomMenuItem> mIStatus = new ArrayList<>();
+    List<CheckBox> cBPositions = new ArrayList<>();
+    List<CheckBox> cBIndustries = new ArrayList<>();
+    List<CustomMenuItem> mIPositions = new ArrayList<>();
+    List<CustomMenuItem> mIIndustries = new ArrayList<>();
 
     List<ApplicantUI> applicantsList;
 
@@ -61,13 +58,14 @@ public class OverviewController {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/storageView.fxml"));
         loader.setController(this);
         root = (Pane) loader.load();
+        setDictionaries();
         setTable();
-        setDictionary();
     }
 
-    private void setDictionary() {
-        biDic = new BasicInfoDictionary();
-        aiDic = new ApplicantInfoDictionary();
+    private void setDictionaries() {
+        biDictionary = new BasicInfoDictionary();
+        aiDictionary = new ApplicantInfoDictionary();
+        pNiDictionary = new PositionsAndIndustriesDictionary();
     }
 
     public void setFactoriesAndComparatorsForTableColumns() {
@@ -82,21 +80,19 @@ public class OverviewController {
                 user ->
                         new ReadOnlyStringWrapper(
                                 IDictionary.getTranslation(
-                                        aiDic, user.getValue().getStatus().toString())));
-        //firstNameCol.setText(IDictionary.getTranslation(biDic, "First Name"));
-        //lastNameCol.setText(IDictionary.getTranslation(biDic, "Last Name"));
+                                        aiDictionary, user.getValue().getStatus().getStatus())));
+        firstNameCol.setText(IDictionary.getTranslation(biDictionary, "First Name"));
+        lastNameCol.setText(IDictionary.getTranslation(biDictionary, "Last Name"));
     }
 
     public ObservableList<ApplicantUI> setTable() {
         applicantsList = model.getDB();
-        Collections.reverse(applicantsList);
         AddClickFunctionToUserTable();
         observableListTableView = userTable.getItems();
         observableListTableView.clear();
-        observableListTableView.addAll(applicantsList);
+        observableListTableView.addAll(sortApplicants(applicantsList));
         setFactoriesAndComparatorsForTableColumns();
         setFilters();
-
 
         return observableListTableView;
     }
@@ -107,92 +103,167 @@ public class OverviewController {
     }
 
     private void setNameListner() {
-        txtNameFX.textProperty().addListener(
-                (observable, oldValue, newValue)->{
-                    if(newValue.equals("")){
-                        applicantsList = model.getDB();
-                        Collections.reverse(applicantsList);
-                        setTable(applicantsList);
-                    }
-                    else{
-                        applicantsList = model.getDB().stream()
-                                .filter(x->((x.getFirstName()+" "+x.getLastName()).toLowerCase().
-                                        contains(newValue.toString().toLowerCase())))
-                                .collect(Collectors.toList());
-                        Collections.reverse(applicantsList);
-                        setTable(applicantsList);
-                    }
-                }
-        );
+        txtNameFX
+                .textProperty()
+                .addListener(
+                        (observable, oldValue, newValue) -> {
+                            applyFilters();
+                        });
+    }
+
+    private void applyFilters() {
+
+        if (txtNameFX.getText().equals("")) {
+            applicantsList = model.getDB();
+
+            setTable(applicantsList);
+        } else {
+            applicantsList =
+                    model.getDB()
+                            .stream()
+                            .filter(
+                                    x ->
+                                            ((x.getFirstName() + " " + x.getLastName())
+                                                    .toLowerCase()
+                                                    .contains(
+                                                            txtNameFX
+                                                                    .getText()
+                                                                    .toString()
+                                                                    .toLowerCase())))
+                            .collect(Collectors.toList());
+
+            setTable(applicantsList);
+        }
+
+        List<ApplicantUI> temp = new ArrayList<>();
+
+        if (cBStatus.stream().filter(x -> x.isSelected()).count() != 0) {
+            temp =
+                    applicantsList
+                            .stream()
+                            .filter(
+                                    app ->
+                                            cBStatus.stream()
+                                                    .filter(x -> x.isSelected())
+                                                    .anyMatch(
+                                                            x ->
+                                                                    x.getText()
+                                                                            .equals(
+                                                                                    IDictionary
+                                                                                            .getTranslation(
+                                                                                                    aiDictionary,
+                                                                                                    app.getStatus()
+                                                                                                            .getStatus()))))
+                            .collect(Collectors.toList());
+
+            applicantsList.clear();
+            applicantsList = new ArrayList<>(temp);
+            temp.clear();
+        }
+        if (cBPositions.stream().filter(x -> x.isSelected()).count() != 0) {
+            temp =
+                    applicantsList
+                            .stream()
+                            .filter(
+                                    app ->
+                                            app.getPositions()
+                                                    .stream()
+                                                    .anyMatch(
+                                                            pos ->
+                                                                    cBPositions
+                                                                            .stream()
+                                                                            .filter(
+                                                                                    cBPos ->
+                                                                                            cBPos
+                                                                                                    .isSelected())
+                                                                            .anyMatch(
+                                                                                    cBPos ->
+                                                                                            cBPos.getText()
+                                                                                                    .equals(
+                                                                                                            IDictionary
+                                                                                                                    .getTranslation(
+                                                                                                                            pNiDictionary,
+                                                                                                                            pos
+                                                                                                                                    .getPosition())))))
+                            .collect(Collectors.toList());
+
+            applicantsList.clear();
+            applicantsList = new ArrayList<>(temp);
+            temp.clear();
+        }
+
+        if (cBIndustries.stream().filter(x -> x.isSelected()).count() != 0) {
+            temp =
+                    applicantsList
+                            .stream()
+                            .filter(
+                                    app ->
+                                            app.getIndustries()
+                                                    .stream()
+                                                    .anyMatch(
+                                                            ind ->
+                                                                    cBIndustries
+                                                                            .stream()
+                                                                            .filter(
+                                                                                    cBInd ->
+                                                                                            cBInd
+                                                                                                    .isSelected())
+                                                                            .anyMatch(
+                                                                                    cBInd ->
+                                                                                            cBInd.getText()
+                                                                                                    .equals(
+                                                                                                            IDictionary
+                                                                                                                    .getTranslation(
+                                                                                                                            pNiDictionary,
+                                                                                                                            ind
+                                                                                                                                    .getIndustry())))))
+                            .collect(Collectors.toList());
+
+            applicantsList.clear();
+            applicantsList = new ArrayList<>(temp);
+            temp.clear();
+        }
+        setTable(applicantsList);
     }
 
     private void setComboBoxes() {
-
-        pNiDictionary = new PositionsAndIndustriesDictionary();
-        aIDictionary = new ApplicantInfoDictionary();
-
-        List<CheckBox> cBStatus = new ArrayList<>();
-        List<CustomMenuItem> mIStatus= new ArrayList<>();
-        cBStatus.add(new CheckBox(IDictionary.getTranslation(aIDictionary,"Open")));
-        mIStatus.add(new CustomMenuItem(cBStatus.get(cBStatus.size()-1)));
-        cBStatus.add(new CheckBox(IDictionary.getTranslation(aIDictionary,"Sent to HR")));
-        mIStatus.add(new CustomMenuItem(cBStatus.get(cBStatus.size()-1)));
-        cBStatus.add(new CheckBox(IDictionary.getTranslation(aIDictionary,"Denied")));
-        mIStatus.add(new CustomMenuItem(cBStatus.get(cBStatus.size()-1)));
-
+        for (Status status : Status.values()) {
+            cBStatus.add(
+                    new CheckBox(IDictionary.getTranslation(aiDictionary, status.getStatus())));
+            mIStatus.add(new CustomMenuItem(cBStatus.get(cBStatus.size() - 1)));
+            cBStatus.get(cBStatus.size() - 1).setOnAction(e -> applyFilters());
+        }
+        for (Positions position : Positions.values()) {
+            cBPositions.add(
+                    new CheckBox(
+                            IDictionary.getTranslation(pNiDictionary, position.getPosition())));
+            mIPositions.add(new CustomMenuItem(cBPositions.get(cBPositions.size() - 1)));
+            cBPositions.get(cBPositions.size() - 1).setOnAction(e -> applyFilters());
+        }
+        for (Industries industry : Industries.values()) {
+            cBIndustries.add(
+                    new CheckBox(
+                            IDictionary.getTranslation(pNiDictionary, industry.getIndustry())));
+            mIIndustries.add(new CustomMenuItem(cBIndustries.get(cBIndustries.size() - 1)));
+            cBIndustries.get(cBIndustries.size() - 1).setOnAction(e -> applyFilters());
+        }
         mBtnStatusFX.getItems().setAll(mIStatus);
-
-        Positions positions[] = Positions.values();
-        Industries insustries[] = Industries.values();
-        List<CheckBox> cBPositions = new ArrayList<>();
-        List<CheckBox> cBIndustries = new ArrayList<>();
-        List<CustomMenuItem> mIPositions = new ArrayList<>();
-        List<CustomMenuItem> mIIndustries = new ArrayList<>();
-
-        for(Positions position: positions){
-
-            cBPositions.add(new CheckBox(IDictionary.getTranslation(pNiDictionary,position.getPosition())));
-            mIPositions.add(new CustomMenuItem(cBPositions.get(cBPositions.size()-1)));
-        }
-
         mBtnPositionFX.getItems().setAll(mIPositions);
-
-        for(Industries industry: insustries){
-
-            cBIndustries.add(new CheckBox(IDictionary.getTranslation(pNiDictionary,industry.getIndustry())));
-            mIIndustries.add(new CustomMenuItem(cBIndustries.get(cBIndustries.size()-1)));
-        }
-
         mBtnIndustryFX.getItems().setAll(mIIndustries);
-
-        for(MenuItem mI: mIStatus){
-            mI.setOnAction(cBListener(cBStatus,cBPositions,cBIndustries));
-        }
-
-    }
-
-    private EventHandler<ActionEvent> cBListener
-            (List<CheckBox> cBStatus, List<CheckBox> cBPositions, List<CheckBox> cBIndustries) {
-
-        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e)
-            {
-
-            }
-        };
-
-        return event;
     }
 
     private void setTable(List<ApplicantUI> applicantUIList) {
 
         observableListTableView = userTable.getItems();
         observableListTableView.clear();
-        observableListTableView.addAll(applicantUIList);
+        observableListTableView.addAll(sortApplicants(applicantUIList));
         setFactoriesAndComparatorsForTableColumns();
     }
 
-
+    private List<ApplicantUI> sortApplicants(List<ApplicantUI> applicantList) {
+        Collections.sort(applicantsList, (a1, a2) -> (int) (a2.getID() - a1.getID()));
+        return applicantList;
+    }
 
     public Pane getPane() {
         return root;
