2d732b1cedee658e9c2c60aee9afdad95785465d
Bharathwaj-Ravi-1995 <bharathravi1995@gmail.com>
2020-07-13 23:09:22 +0530

    Login action handled

--- frontend/src/app/login/login.page.ts
+++ frontend/src/app/login/login.page.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { StorageHandlerService } from '../services/storage-handler.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  userName: string;
+  password: string;
+  apiHostUrl = 'http://localhost:9090';
 
-  constructor() { }
+  constructor(
+    private navController: NavController,
+    private httpClient: HttpClient,
+    private storage: StorageHandlerService
+  ) {}
 
-  ngOnInit() {
+  skip(): void {
+    this.navController.navigateForward(['/home']);
   }
 
+  login(): void {
+    this.httpClient
+      .post(
+        this.apiHostUrl + '/api/controller/register',
+        { userName: this.userName, password: this.password },
+        {
+          responseType: 'text',
+          observe: 'response',
+        }
+      )
+      .subscribe(
+        (res) => {
+          if (res.status === 201) {
+            this.storage.addItem(
+              this.storage.commonPropertiesDb,
+              'deviceToken',
+              res.headers.get('deviceauthorization')
+            );
+            this.storage.addItem(
+              this.storage.commonPropertiesDb,
+              'userToken',
+              res.headers.get('userauthorization')
+            );
+            this.navController.navigateForward(['/home']);
+          }
+        },
+        () => {
+          alert('Not autheticated');
+        }
+      );
+  }
 }
