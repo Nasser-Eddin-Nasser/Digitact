1a591ea596c7c78f21b3ff703b08b0d6de936f27
Nasser-Eddin-Nasser <nassereddin.nasser@fau.de>
2020-07-17 18:09:16 +0200

    add remove unique methode to avoid the bug

--- Backend/src/main/java/Digitact/Backend/Util/PasswordTools.java
+++ Backend/src/main/java/Digitact/Backend/Util/PasswordTools.java
@@ -5,15 +5,25 @@ import java.security.NoSuchAlgorithmException;
 
 public class PasswordTools {
 
-    public static String encryptString(String password) {
-        MessageDigest messageDigest = null;
-        try {
-            messageDigest = MessageDigest.getInstance("SHA-256");
-            messageDigest.update(password.getBytes());
-            return new String(messageDigest.digest());
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return null;
+  public static String encryptString(String password) {
+    MessageDigest messageDigest = null;
+    try {
+      messageDigest = MessageDigest.getInstance("SHA-256");
+      messageDigest.update(password.getBytes());
+      return new String(messageDigest.digest());
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
+
+  public static String removeSpecialCharacters(String password) {
+    String resultStr = "";
+    for (int i = 0; i < password.length(); i++) {
+      if (password.charAt(i) > 64 && password.charAt(i) <= 122) {
+        resultStr = resultStr + password.charAt(i);
+      }
+    }
+    return resultStr;
+  }
 }
